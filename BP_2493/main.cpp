99331867b5eb1e4f48bdacb9f9e5a5aa17b931ff
Yeonji-Lim <view7186@naver.com>
2020-10-08 15:17:56 +0900

    Solved

--- BP_2493/main.cpp
+++ BP_2493/main.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <deque>
+#include <stack>
 using namespace std;
 
-//반복문을 하나 더 줄여볼 수 있을까..?
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int N = 0;
     int h = 0;
-    deque<int> DQ;
+    stack<pair<int, int>> S;
     int ans = 0;
 
-    scanf("%d", &N);
+    cin >> N;
     for(int i=0; i<N; i++) {
-        scanf("%d", &h);
-        if(DQ.empty()) {
-            DQ.push_back(h);
-        }
-        else {
-            for(int j=0; j<DQ.size(); j++) {
-                if(DQ[j] > h) ans = j+1;
-            }
-            DQ.push_back(h);
-        }
+        cin >> h;
+        while(!S.empty() && S.top().first < h)
+            S.pop();
+        if(!S.empty()) ans = S.top().second + 1;
+        S.push(make_pair(h,i));
         printf("%d ", ans);
+        ans = 0;
     }
     printf("\n");
     return 0;
--- BP_5430/main.cpp
+++ BP_5430/main.cpp
@@ -1,63 +1,88 @@
 #include <iostream>
-#include <deque>
+//#include <deque>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
 
     int T;
     string p;
     int n;
     string listStr;
-    int data;
-    deque<int> DQ;
+    int rev;
+//    deque<int> DQ;
 
     cin >> T;
-    for(int j=0; j<T; j++) {
+    while(T--) {
         cin >> p >> n >> listStr;
-        // DQ에 원소 삽입
-        listStr = listStr.substr(1, listStr.length() - 2) + ","; // []빼고 일관성을 위해 마지막에 , 추가
-        while(listStr.length() > 1) { //1인 경우는 ',' 하나 밖에 없다는 거니까 empty라는 거
-            data = stoi(listStr.substr(0, listStr.find(',')));
-            DQ.push_back(data);
-            listStr = listStr.substr(listStr.find(',') + 1);
-        }
-        // 명령 수행
-        data = 0;
-        for(int i=0; i<p.length(); i++) {
-            if(p[i] == 'R') {   //data 변수 재활용, 0이면 front가 front, 1이면 back이 front
-                data == 0? data = 1 : data = 0;
-            } else {    // 버리기 명령
-                if(DQ.empty()) {
-                    n = -1;
-                    break;
+        listStr = listStr.substr(1,listStr.length()-2); // []빼기
+        rev = 0;
+        while(p.length() != 0) {
+            if(p[0] == 'R') {
+                rev == 0? rev = 1: rev = 0;
+            } else {
+                if(listStr.length() == 0) break;
+                if(listStr.length() == 1) listStr.clear(); // 원소가 한개인 경우 ','가 없음
+                else if(rev == 0) {
+                    listStr = listStr.substr(2,listStr.length()-2);
+                } else {
+                    listStr.pop_back();
+                    listStr.pop_back();
                 }
-                data == 0? DQ.pop_front() : DQ.pop_back();
             }
+            p = p.substr(1);
         }
-        //프린트
-        if(n == -1) {
-            cout << "error\n";
-            continue;
+        if(listStr.empty()) printf("error\n");
+        else {
+            if(rev == 1) reverse(listStr.begin(), listStr.end());
+            listStr = "[" +listStr+ "]";
+            printf("%s\n", listStr.c_str());
         }
-        cout << "[";
-        while(data == 0 && !DQ.empty()) {
-            cout << DQ.front();
-            DQ.pop_front();
-            if(DQ.empty()) break;
-            cout << ",";
-        }
-        while(data == 1 && !DQ.empty()) {
-            cout << DQ.back();
-            DQ.pop_back();
-            if(DQ.empty()) break;
-            cout << ",";
-        }
-        cout << "]\n";
-        DQ.clear();
+
+        // 1차 시도 : string으로 안하고 받아서 따로 저장하려 함
+//        // DQ에 원소 삽입
+//        listStr = listStr.substr(1, listStr.length() - 2) + ","; // []빼고 일관성을 위해 마지막에 , 추가
+//        while(listStr.length() > 1) { //1인 경우는 ',' 하나 밖에 없다는 거니까 empty라는 거
+//            data = stoi(listStr.substr(0, listStr.find(',')));
+//            DQ.push_back(data);
+//            listStr = listStr.substr(listStr.find(',') + 1);
+//        }
+//        // 명령 수행
+//        data = 0;
+//        for(int i=0; i<p.length(); i++) {
+//            if(p[i] == 'R') {   //data 변수 재활용, 0이면 front가 front, 1이면 back이 front
+//                data == 0? data = 1 : data = 0;
+//            } else {    // 버리기 명령
+//                if(DQ.empty()) {
+//                    n = -1;
+//                    break;
+//                }
+//                data == 0? DQ.pop_front() : DQ.pop_back();
+//            }
+//        }
+//        //프린트
+//        if(n == -1) {
+//            cout << "error\n";
+//            continue;
+//        }
+//        cout << "[";
+//        while(data == 0 && !DQ.empty()) {
+//            cout << DQ.front();
+//            DQ.pop_front();
+//            if(DQ.empty()) break;
+//            cout << ",";
+//        }
+//        while(data == 1 && !DQ.empty()) {
+//            cout << DQ.back();
+//            DQ.pop_back();
+//            if(DQ.empty()) break;
+//            cout << ",";
+//        }
+//        cout << "]\n";
+//        DQ.clear();
     }
     return 0;
 }
