e8cace1ac5ac2f8a1318b1787a7560e1d0d46656
Yeonji-Lim <view7186@naver.com>
2020-10-24 23:47:33 +0900

    Solved

--- BOJ_1789/cmake-build-debug/CMakeFiles/BP_1789.dir/DependInfo.cmake
+++ BOJ_1789/cmake-build-debug/CMakeFiles/BP_1789.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BP_1789/main.cpp" "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BP_1789/cmake-build-debug/CMakeFiles/BP_1789.dir/main.cpp.o"
+  "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BOJ_1789/main.cpp" "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BOJ_1789/cmake-build-debug/CMakeFiles/BP_1789.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- BOJ_1789/main.cpp
+++ BOJ_1789/main.cpp
@@ -1,24 +1,15 @@
-#include <iostream>
-using namespace std;
-
+#include <cstdio>
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    unsigned int S;
+    long long S, sum = 0;
     int N = 1;
-    int L = 1, R;
-    int mid;
-    cin >> S;
-    R = S;
-    while(L<=R) {
-        mid = (L+R)/2;
-        if(S >= mid*(mid+1)/2) {
-            if(mid > N) N = mid;
-            L = mid+1;
-        } else {
-            R = mid-1;
+    scanf("%lld", &S);
+    while(true) {
+        sum += N;
+        if(sum > S) {
+            N--;
+            break;
         }
+        N++;
     }
     printf("%d", N);
     return 0;
