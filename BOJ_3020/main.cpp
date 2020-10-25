0cd856551de27f9c7a364d0464eb61b151d08257
Yeonji-Lim <view7186@naver.com>
2020-10-25 12:42:07 +0900

    Time out

--- BOJ_3020/main.cpp
+++ BOJ_3020/main.cpp
@@ -1,71 +1,29 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-vector<int> rstV;
-
-int LowerBound(int data, vector<int> V) {
-    int L=0, R=V.size();
-    int mid;
-    int result;
-    while(L<=R) {
-        mid = (L+R)/2;
-        if(data >= V[mid]) {
-            if(data == V[mid]) result = mid;
-            R = mid-1;
-        } else L = mid+1;
-    }
-    return result;
-}
-
-int UpperBound(int data) {
-    int L=0, R=rstV.size();
-    int mid;
-    int result;
-    while(L<=R) {
-        mid = (L+R)/2;
-        if(data <= rstV[mid]) {
-            if(data == rstV[mid]) result = mid;
-            L = mid+1;
-        } else R = mid-1;
-    }
-    return result;
-}
-
 int main() {
-    int N, H;
-    int obs;
-    int result = 1;
-    vector<int> S;
-    vector<int> J;
-    cin >> N >> H;
-    S.resize(N/2, 0);
-    J.resize(N/2,0);
-    for(int i=0; i<N; i++) {
-        cin >> obs;
-        if(i%2 != 0) { // 종유석
-            J.push_back(obs);
-        } else { // 석순
-            S.push_back(obs);
+    int N, H, obs;
+    scanf("%d %d", &N, &H);
+    vector<int> V (H, 0);
+    while(N--) {
+        scanf("%d", &obs);
+        if(N%2) {
+            while(obs) {
+                ++V[H-obs];
+                obs--;
+            }
+        } else {
+            while(obs) {
+                ++V[obs-1];
+                obs--;
+            }
         }
     }
-    sort(S.begin(), S.end());
-    sort(J.begin(), J.end());
-//    N = (S.size()-LowerBound(H/2,S))+(J.size()-LowerBound(H-H/2+1,J));
-    int j;
-    for(int i=0; i<H; i++) {
-        j = 0;
-        while(S[j] < i) j++;
-        obs = S.size() - j;
-        j = 0;
-        while(J[j] < H-i+1) j++;
-        obs += J.size() - j;
-        rstV.push_back(obs);
-    }
-    sort(rstV.begin(), rstV.end());
-    j = 0;
-    while(rstV[j] == rstV.front()) j++;
-    printf("%d", j-1);
+    sort(V.begin(), V.end());
+    obs = 0;
+    while(V[0] == V[obs]) obs++;
+    printf("%d %d", V[0], obs);
     return 0;
 }
