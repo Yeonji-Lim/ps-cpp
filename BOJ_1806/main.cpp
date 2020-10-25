9a1f16b62bb2d5bbe286be7d6abe3333150353ee
Yeonji-Lim <view7186@naver.com>
2020-10-25 11:47:25 +0900

    Failed

--- BOJ_1806/main.cpp
+++ BOJ_1806/main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+#include <cstdio>
 #include <vector>
-#include <queue>
 using namespace std;
 int N, S;
 
@@ -10,18 +9,13 @@ int main() {
     for(int i=0; i<N; i++) {
         scanf("%d", &V[i]);
     }
-    queue<int> Q;
+    int l = 0, h = 0;
     int sum = 0, ans = N+1;
-    for(int i=0; i<N; i++) {
-        Q.push(V[i]);
-        sum+=V[i];
-        if(sum > S) {
-            while(sum-Q.front() > S) {
-                sum-=Q.front();
-                Q.pop();
-            }
-            if(Q.size() < ans) ans = Q.size();
-        }
+    while(l <= h && h < N) {
+        sum += V[h];
+        while(sum-V[l] > S) sum -= V[l++];
+        if(sum >= S && ans > h-l+1) ans = h-l+1;
+        h++;
     }
     ans == N+1? printf("0"): printf("%d", ans);
     return 0;
