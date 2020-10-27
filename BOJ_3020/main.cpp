d3504a443d00117f51beacdb10c2fce8aa2c1cde
Yeonji-Lim <view7186@naver.com>
2020-10-27 20:23:33 +0900

    Solved

--- BOJ_3020/main.cpp
+++ BOJ_3020/main.cpp
@@ -6,24 +6,30 @@ using namespace std;
 int main() {
     int N, H, obs;
     scanf("%d %d", &N, &H);
-    vector<int> V (H, 0);
-    while(N--) {
+    vector<int> J (H,0);
+    vector<int> S (H,0);
+    while(N) {
         scanf("%d", &obs);
         if(N%2) {
-            while(obs) {
-                ++V[H-obs];
-                obs--;
-            }
+            J[obs-1]++;
         } else {
-            while(obs) {
-                ++V[obs-1];
-                obs--;
-            }
+            S[obs-1]++;
+        }
+        N--;
+    }
+    for(int i=1; i<H; i++) {
+        J[H-i-1] += J[H-i];
+        S[H-i-1] += S[H-i];
+    }
+    for(int i=0; i<H; i++) {
+        J[i] += S[H-i-1];
+    }
+    sort(J.begin(), J.end());
+    for(int i=0; i<H; i++) {
+        if(J[0] != J[i]) {
+            printf("%d %d", J[0], i);
+            break;
         }
     }
-    sort(V.begin(), V.end());
-    obs = 0;
-    while(V[0] == V[obs]) obs++;
-    printf("%d %d", V[0], obs);
     return 0;
 }
