ce64c9b77d96c990031a89f7e345f797b99af5f0
Yeonji-Lim <view7186@naver.com>
2020-11-20 15:36:04 +0900

    Solved

--- BOJ_1463/main.cpp
+++ BOJ_1463/main.cpp
@@ -1,48 +1,31 @@
 #include <cstdio>
-#include <vector>
-#define MAX_LIMIT 1000000
 using namespace std;
 int N;
-vector<int> V;
+int A[1000001];
 
-int calculate(int n) {
-    int rst = MAX_LIMIT;
-    int tmp;
-    if(n<4) {
-        return V[n];
-    }
-    if(n%3 == 0) {
-        V[n / 3] != 0 ? rst = V[n / 3] : rst = calculate(n / 3);
-    }
-    if(n%2 == 0) {
-        if(V[n/2] != 0) {
-            if(rst > V[n / 2]) rst = V[n / 2];
-        } else {
-            tmp = calculate(n/2);
-            if(rst > tmp) rst = tmp;
+int min(int a, int b) {
+    return a > b? b: a;
+}
+
+void calculate(int n) {
+    if(n<4) return;
+    for(int i=3; i<n+1; i++) {
+        int rst = A[i-1] + 1;
+        if(i%3 == 0) {
+            rst = min(rst, A[i/3] + 1);
         }
-    }
-    if(V[n-1] != 0) {
-        if(rst > V[n-1]) {
-            rst = V[n-1];
+        if(i%2 == 0) {
+            rst = min(rst, A[i/2] + 1);
         }
-    } else {
-        tmp = calculate(n-1);
-        if(rst > tmp) {
-            rst = tmp;
-        }
+        A[i] = rst;
     }
-    rst++;
-    V[n] = rst;
-    return rst;
 }
 
 int main() {
     scanf("%d", &N);
-    V.assign(N+1,0);
-    V[2] = 1;
-    V[3] = 1;
+    A[2] = 1;
+    A[3] = 1;
     calculate(N);
-    printf("%d", V[N]);
+    printf("%d", A[N]);
     return 0;
 }
