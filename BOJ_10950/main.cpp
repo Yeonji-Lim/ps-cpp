2e6582e2d12387654c2702b3215f51ea4a3094f4
Yeonji-Lim <view7186@naver.com>
2021-02-18 11:23:15 +0900

    Solved

---
+++ BOJ_10950/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int i, T, A, B;
+    scanf("%d", &T);
+    for(i = 0; i < T; i++) {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A + B);
+    }
+    return 0;
+}
