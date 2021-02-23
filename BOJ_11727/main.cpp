541a657a0829de7972d2e3ac9545c00860e3e1db
Yeonji-Lim <view7186@naver.com>
2021-02-23 20:20:35 +0900

    Solved - %10007 빼먹음

--- BOJ_11727/main.cpp
+++ BOJ_11727/main.cpp
@@ -5,7 +5,7 @@ int cache[1001];
 int tiling2(int n) {
     int & rst = cache[n];
     if(rst != 0) return rst;
-    rst = tiling2(n-1) + 2*tiling2(n-2);
+    rst = (tiling2(n-1) + 2*tiling2(n-2)) % 10007;
     return rst;
 }
 
