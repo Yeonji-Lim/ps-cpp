60fa675273e408b7ad84f7d26f460e1fe00db67c
Yeonji-Lim <view7186@naver.com>
2021-02-23 20:18:53 +0900

    Failed

---
+++ BOJ_11727/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstring>
+int cache[1001];
+
+int tiling2(int n) {
+    int & rst = cache[n];
+    if(rst != 0) return rst;
+    rst = tiling2(n-1) + 2*tiling2(n-2);
+    return rst;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    memset(cache, 0, sizeof(cache));
+    cache[1] = 1;
+    cache[2] = 3;
+    printf("%d", tiling2(n));
+}
