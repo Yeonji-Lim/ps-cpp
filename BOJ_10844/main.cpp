620723594959dd057564b851ba38adf68f64e39d
Yeonji-Lim <view7186@naver.com>
2021-02-24 13:38:06 +0900

    Failed

---
+++ BOJ_10844/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cstring>
+#define MOD 1000000000
+int cache[101];
+
+int numOfStairsNumber(int n) {
+    int & ret = cache[n];
+    if(ret != 0) return ret;
+    ret = ( 2 * numOfStairsNumber(n-1) - 2 + numOfStairsNumber(n-2) ) % MOD;
+    return ret;
+}
+
+int main() {
+    memset(cache, 0, sizeof(cache));
+    cache[1] = 9;
+    cache[2] = 17;
+    int N;
+    scanf("%d", &N);
+    printf("%d", numOfStairsNumber(N));
+    return 0;
+}
