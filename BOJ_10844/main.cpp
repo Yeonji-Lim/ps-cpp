643f6ff7a43eddf43b4596a07901ef0f9acfab78
Yeonji-Lim <view7186@naver.com>
2021-02-24 16:54:50 +0900

    Failed

--- BOJ_10844/main.cpp
+++ BOJ_10844/main.cpp
@@ -1,21 +1,37 @@
 #include <cstdio>
 #include <cstring>
 #define MOD 1000000000
-int cache[101];
+int cache[101][10];
 
-int numOfStairsNumber(int n) {
-    int & ret = cache[n];
-    if(ret != 0) return ret;
-    ret = ( 2 * numOfStairsNumber(n-1) - (n-1)) % MOD;
-    return ret;
+void stairsNumber(int n) {
+    if(cache[n][1] != 0) {
+        return;
+    } else if(cache[n-1][0] == 0) {
+        stairsNumber(n-1);
+    }
+    for(int i = 0; i < 10; i++) {
+        if(i == 0) {
+            cache[n][i] = cache[n-1][1];
+        } else if(i == 9) {
+            cache[n][i] = cache[n-1][8];
+        } else {
+            cache[n][i] = (cache[n-1][i+1] + cache[n-1][i-1]) % MOD;
+        }
+    }
+    return;
 }
 
 int main() {
     memset(cache, 0, sizeof(cache));
-    cache[1] = 9;
-    cache[2] = 17;
-    int N;
+    for(int i = 1; i < 10; i++) {
+        cache[1][i] = 1;
+    }
+    int N, rst = 0;
     scanf("%d", &N);
-    printf("%d", numOfStairsNumber(N));
+    stairsNumber(N);
+    for(int i = 0; i < 10; i++) {
+        rst += cache[N][i];
+    }
+    printf("%d", rst % MOD);
     return 0;
 }
