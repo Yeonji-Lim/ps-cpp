bc3daf16e4c70a7706d9edba9e3002588fa13dcf
Yeonji-Lim <view7186@naver.com>
2021-02-25 13:49:17 +0900

    입력 테스트

--- BOJ_9465/main.cpp
+++ BOJ_9465/main.cpp
@@ -14,34 +14,40 @@ void sticker(int n) {
 }
 
 int main() {
-    int t, n, i, j, rst;
-    scanf("%d", &t);
-    for(i = 0; i < t; i++) {
+    int n;
+    for(int i = 0; i < 10; i++) {
         scanf("%d", &n);
-
-        for(j = 2; j < ARR_SIZE; j++) {
-            memset(DP[j], -1, sizeof(DP[0]));
-        }
-        memset(sti[0], -1, sizeof(sti[0]));
-        memset(sti[1], -1, sizeof(sti[1]));
-
-        for(j = 1; j <= n; j++) {
-            scanf("%d", sti[0][j]);
-        }
-        for(j = 1; j <= n; j++) {
-            scanf("%d", sti[1][j]);
-        }
-
-        DP[1][0] = sti[0][1];
-        DP[1][1] = sti[1][1];
-        DP[1][2] = 0;
-
-        printf("%d %d %d", DP[2][0], sti[0][1], sti[1][1]);
-        sticker(n);
-
-        rst = DP[n][0] > DP[n][1]? DP[n][0] : DP[n][1];
-        rst = rst > DP[n][2]? rst : DP[n][2];
-        printf("%d\n", rst);
+        printf("%d\n", n);
     }
+
+//    int t, n, i, j, rst;
+//    scanf("%d", &t);
+//    for(i = 0; i < t; i++) {
+//        scanf("%d", &n);
+//
+//        for(j = 2; j < ARR_SIZE; j++) {
+//            memset(DP[j], -1, sizeof(DP[0]));
+//        }
+//        memset(sti[0], -1, sizeof(sti[0]));
+//        memset(sti[1], -1, sizeof(sti[1]));
+//
+//        for(j = 1; j <= n; j++) {
+//            scanf("%d", sti[0][j]);
+//        }
+//        for(j = 1; j <= n; j++) {
+//            scanf("%d", sti[1][j]);
+//        }
+//
+//        DP[1][0] = sti[0][1];
+//        DP[1][1] = sti[1][1];
+//        DP[1][2] = 0;
+//
+//        printf("%d %d %d", DP[2][0], sti[0][1], sti[1][1]);
+//        sticker(n);
+//
+//        rst = DP[n][0] > DP[n][1]? DP[n][0] : DP[n][1];
+//        rst = rst > DP[n][2]? rst : DP[n][2];
+//        printf("%d\n", rst);
+//    }
     return 0;
 }
