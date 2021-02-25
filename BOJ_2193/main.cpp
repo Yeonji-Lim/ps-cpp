87e4326582865f10565d2393ef4a78ff0ce2dd67
Yeonji-Lim <view7186@naver.com>
2021-02-25 12:30:32 +0900

    Failed

--- BOJ_2193/main.cpp
+++ BOJ_2193/main.cpp
@@ -1,12 +1,41 @@
+//#include <cstdio>
+//int n;
+//long long int A[91] = {0, 1};
+//
+//int main() {
+//    scanf("%d", &n);
+//    for(int i=2; i<n+1; i++) {
+//        A[i] = A[i-1] + A[i-2];
+//    }
+//    printf("%lld", A[n]);
+//    return 0;
+//}
+
+// 2021.2.25
 #include <cstdio>
-int n;
-long long int A[91] = {0, 1};
+#include <cstring>
+int cache[91][2];
+
+void bimaryNumber(int n) {
+    if(cache[n][0] != -1) return;
+    if(cache[n-1][0] == -1) {
+        bimaryNumber(n-1);
+    }
+    cache[n][0] = cache[n-1][0] + cache[n-1][1];
+    cache[n][1] = cache[n-1][0];
+    return;
+}
 
 int main() {
-    scanf("%d", &n);
-    for(int i=2; i<n+1; i++) {
-        A[i] = A[i-1] + A[i-2];
+    int n;
+    for(int i = 0; i < 91; i++) {
+        memset(cache, -1, sizeof(cache));
     }
-    printf("%lld", A[n]);
+    cache[1][0] = 0;
+    cache[1][1] = 1;
+    scanf("%d", &n);
+    bimaryNumber(n);
+    printf("%d", cache[n][0] + cache[n][1]);
     return 0;
 }
+
