283bee9f0c27679411ccb5962cbfb2482d48645d
Yeonji-Lim <view7186@naver.com>
2021-03-05 16:35:43 +0900

    Failed - n이 증가할 때 마다 새로운 케이스가 생기는 것을 간과함

--- BOJ_2133/main.cpp
+++ BOJ_2133/main.cpp
@@ -1,25 +1,27 @@
-#include <cstdio>
-#include <cstring>
-#define MAX 31
-int dp[MAX];
+/* 2021.3.4 Failed */
+//#include <cstdio>
+//#include <cstring>
+//#define MAX 31
+//int dp[MAX];
+//
+//int tiling(int n) {
+//    int & ret = dp[n];
+//    if(ret != 0) return ret;
+//    ret = 3*tiling(n-1) + 2*tiling(n-2);
+//    return ret;
+//}
+//
+//int main() {
+//    int n;
+//    memset(dp, 0, sizeof(dp));
+//    scanf("%d", &n);
+//    if(n%2 == 1) {
+//        printf("0\n");
+//    } else {
+//        dp[0] = 1;
+//        dp[1] = 3;
+//        printf("%d\n", tiling(n/2));
+//    }
+//    return 0;
+//}
 
-int tiling(int n) {
-    int & ret = dp[n];
-    if(ret != 0) return ret;
-    ret = 3*tiling(n-1) + 2*tiling(n-2);
-    return ret;
-}
-
-int main() {
-    int n;
-    memset(dp, 0, sizeof(dp));
-    scanf("%d", &n);
-    if(n%2 == 1) {
-        printf("0\n");
-    } else {
-        dp[0] = 1;
-        dp[1] = 3;
-        printf("%d\n", tiling(n/2));
-    }
-    return 0;
-}
