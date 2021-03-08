198e8b02e681f2ce320aca07d1ae5a2dcd472bbc
Yeonji-Lim <view7186@naver.com>
2021-03-08 23:20:19 +0900

    dp[i] 계산식이 틀렸음 num[i]+dp[j]와 비교해야함 비슷한 문제를 풀었다고 너무 그것만 생각하고 풀어버리지 말자

--- BOJ_11055/main.cpp
+++ BOJ_11055/main.cpp
@@ -1,13 +1,13 @@
 #include <cstdio>
 #define MAX 1000
-int dp[MAX];
-int num[MAX];
 
 int max(int a, int b) { return a > b? a: b; }
 
 int main() {
     int n, tmp, i, j;
     scanf("%d", &n);
+    int dp[MAX] = {0};
+    int num[MAX] = {0};
     for(i = 0; i < n; i++) {
         scanf("%d", &tmp);
         num[i] = tmp;
@@ -17,11 +17,11 @@ int main() {
     for(i = 0; i < n; i++) {
         for(j = 0; j < i; j++) {
             if(num[i] > num[j]) {
-                dp[i] = max(dp[i], dp[i]+num[j]);
+                dp[i] = max(dp[i], num[i] + dp[j]);
             }
         }
         tmp = max(tmp, dp[i]);
     }
-    printf("%d", tmp);
+    printf("%d\n", tmp);
     return 0;
 }
