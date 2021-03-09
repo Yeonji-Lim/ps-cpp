8e97c797362e2b0aace951837834a2404d854d5a
Yeonji-Lim <view7186@naver.com>
2021-03-09 20:30:36 +0900

    암호에서 단어를 만들 수 없을 때를 고려하지 않아서 추가했지만 틀렸습니다가 나왔다.

--- BOJ_2011/main.cpp
+++ BOJ_2011/main.cpp
@@ -1,34 +1,38 @@
 #include <cstdio>
 #include <cstring>
 #define MOD 1000000
-#define MAX 5000
+#define MAX 5001
 int dp[MAX];
 char code[MAX];
 
 bool pairPossible(int n) {
-    return ((code[n-1] - '0') * 10 + code[n] - '0') < 27;
+    int tmp = (code[n-1] - '0') * 10 + code[n] - '0';
+    return tmp < 27 || 0 < tmp;
 }
 
 int numOfDecode(int n) {
     int & ret = dp[n];
-    if(ret != 0) return ret;
-    if(pairPossible(n)) {
-        ret = (numOfDecode(n-1) + numOfDecode(n-2)) % MOD;
-    } else {
+    if(ret != -1) return ret;
+    ret = 0;
+    if(code[n-1] != '0') {
         ret = numOfDecode(n-1);
+    }
+    if(pairPossible(n-1)) {
+        ret = (ret + numOfDecode(n-2)) % MOD;
     }
     return ret;
 }
 
 int main() {
-    memset(dp, 0, sizeof(dp));
+    memset(dp, -1, sizeof(dp));
     scanf("%s", code);
+
     dp[0] = 1;
-    if(pairPossible(1)) {
-        dp[1] = 2;
-    } else {
-        dp[1] = 1;
-    }
-    printf("%d\n", numOfDecode(strlen(code)-1));
+
+    if(code[0] != '0') { dp[1] = 1; }
+    else { dp[1] = 0; }
+
+    printf("%d\n", numOfDecode(strlen(code)));
+
     return 0;
 }
