250a85a053aa9a5102eb5d0cea6917fb40bfd78b
Yeonji-Lim <view7186@naver.com>
2021-03-12 15:39:39 +0900

    .

--- BOJ_10828/cmake-build-debug/cmake_install.cmake
+++ BOJ_10828/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BP_10828
+# Install script for directory: /Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp/BOJ_10828
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp14/BP_10828/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/im-yeonji/Documents/GitHub/BOJ_Solution_Cpp/BOJ_10828/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- BOJ_10828/main.cpp
+++ BOJ_10828/main.cpp
@@ -1,36 +1,38 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+/* Solved - 5032KB 16ms */
+//#include <iostream>
+//#include <stack>
+//using namespace std;
+//
+//int main() {
+//    int N;
+//    string command;
+//    stack<int> S;
+//    int data;
+//    cin >> N;
+//    for(int i=0; i<N; i++) {
+//        cin >> command;
+//        if(command == "push") {
+//            cin >> data;
+//            S.push(data);
+//        } else if(command == "pop") {
+//            if(!S.empty()) {
+//                printf("%d\n", S.top());
+//                S.pop();
+//            } else {
+//                printf("-1\n");
+//            }
+//        } else if(command == "top") {
+//            if(!S.empty()) {
+//                printf("%d\n", S.top());
+//            } else {
+//                printf("-1\n");
+//            }
+//        } else if(command == "size") {
+//            printf("%d\n", S.size());
+//        } else if(command == "empty") {
+//            printf("%d\n", S.empty());
+//        }
+//    }
+//    return 0;
+//}
 
-int main() {
-    int N;
-    string command;
-    stack<int> S;
-    int data;
-    cin >> N;
-    for(int i=0; i<N; i++) {
-        cin >> command;
-        if(command == "push") {
-            cin >> data;
-            S.push(data);
-        } else if(command == "pop") {
-            if(!S.empty()) {
-                printf("%d\n", S.top());
-                S.pop();
-            } else {
-                printf("-1\n");
-            }
-        } else if(command == "top") {
-            if(!S.empty()) {
-                printf("%d\n", S.top());
-            } else {
-                printf("-1\n");
-            }
-        } else if(command == "size") {
-            printf("%d\n", S.size());
-        } else if(command == "empty") {
-            printf("%d\n", S.empty());
-        }
-    }
-    return 0;
-}
