ee5b842fd772f722a617fc2039ec504e1c3412a3
Yeonji-Lim <view7186@naver.com>
2021-03-18 13:22:45 +0900

    error

--- BOJ_10814/main.cpp
+++ BOJ_10814/main.cpp
@@ -42,3 +42,66 @@
 //    }
 //    return 0;
 //}
+
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+vector<pair<int, char*>> merge (vector<pair<int, char*>> v1, vector<pair<int, char*>> v2) {
+    vector<pair<int, char*>> ret;
+    int i = 0, j = 0;
+    while(i != v1.size() && j != v2.size()) {
+        if(v1[i].first < v2[j].first) {
+            ret.push_back(v1[i]);
+            i++;
+        } else if(v1[i].first > v2[j].first) {
+            ret.push_back(v2[j]);
+            j++;
+        } else {
+            if(i < j) {
+                ret.push_back(v1[i]);
+                i++;
+            } else {
+                ret.push_back(v2[j]);
+                j++;
+            }
+        }
+    }
+    while(i != v1.size()) {
+        ret.push_back(v1[i]);
+        i++;
+    }
+    while(j != v2.size()) {
+        ret.push_back(v2[j]);
+        j++;
+    }
+    return ret;
+}
+
+vector<pair<int, char*>> customSort (vector<pair<int, char*>> v) {
+    if(v.size() == 1 || v.empty()) return v;
+    vector<pair<int, char*>> ret, l, r;
+    l.insert(l.end(), v.begin(), v.begin()+v.size()/2);
+    r.insert(r.end(), v.begin()+v.size()/2, v.end());
+    l = customSort(l);
+    r = customSort(r);
+    ret = merge(l, r);
+    return ret;
+}
+
+
+int main() {
+    int n, i;
+    pair<int, char*> p = make_pair(0, "0");
+    vector<pair<int, char*>> v;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        scanf("%d %s", &(p.first), &(p.second));
+        v.push_back(p);
+    }
+    v = customSort(v);
+    for(i = 0; i < n; i++) {
+        printf("%d %s\n", v[i].first, v[i].second);
+    }
+    return 0;
+}
