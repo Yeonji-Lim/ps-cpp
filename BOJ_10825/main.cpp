14f42a5d14df3589a95d4fc1ac19bf785b78ad57
Yeonji-Lim <view7186@naver.com>
2021-03-18 20:04:05 +0900

    문자열 사전순 정렬할 때 그냥 단순히 부등호로 비교해도 무방, cstring 헤더 추가안해서 컴파일 에러였음

--- BOJ_10825/main.cpp
+++ BOJ_10825/main.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
-struct data {
+struct Data {
     string name;
     int ko;
     int en;
     int ma;
 };
 
-int rankName (string name) {
-    int ret = 0;
-    for(int i = 0; name[i] != '\0'; i++) {
-        ret += (int)name[i];
-    }
-    return ret;
-}
-
-vector<data> merge (vector<data> v1, vector<data> v2) {
-    vector<data> ret;
+vector<Data> merge (vector<Data> v1, vector<Data> v2) {
+    vector<Data> ret;
     int i = 0, j = 0;
     while(i != v1.size() && j != v2.size()) {
         if(v1[i].ko > v2[j].ko) {
@@ -41,7 +34,7 @@ vector<data> merge (vector<data> v1, vector<data> v2) {
                     ret.push_back(v2[j]);
                     j++;
                 } else {
-                    if(rankName(v1[i].name) < rankName(v2[j].name)) {
+                    if(v1[i].name < v2[j].name) {
                         ret.push_back(v1[i]);
                         i++;
                     } else {
@@ -63,9 +56,9 @@ vector<data> merge (vector<data> v1, vector<data> v2) {
     return ret;
 }
 
-vector<data> customSort (vector<data> v) {
+vector<Data> customSort (vector<Data> v) {
     if(v.size() == 1 || v.empty()) return v;
-    vector<data> ret, l, r;
+    vector<Data> ret, l, r;
     l.insert(l.end(), v.begin(), v.begin()+v.size()/2);
     r.insert(r.end(), v.begin()+v.size()/2, v.end());
     l = customSort(l);
@@ -80,7 +73,7 @@ int main() {
     cout.tie(NULL);
     int n, i;
     scanf("%d", &n);
-    vector<data> v(n);
+    vector<Data> v(n);
     for(i = 0; i < n; i++) {
         cin >> v[i].name >> v[i].ko >> v[i].en >> v[i].ma;
     }
