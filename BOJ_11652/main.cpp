4517ca5e2f1d7319392f9bb2b4937da5ce888dda
Yeonji-Lim <view7186@naver.com>
2021-03-20 17:53:47 +0900

    정수 범위가 long long의 범위도 훨씬 넘어감, 그래서 string으로 받고 숫자 비교를 시도함

--- BOJ_11652/main.cpp
+++ BOJ_11652/main.cpp
@@ -1,9 +1,74 @@
-#include <cstdio>
+/* 2021.3.20 Failed - 정수 숫자 범위 고려 안함 */
+//#include <cstdio>
+//#include <vector>
+//using namespace std;
+//vector<pair<float, int>> v;
+//
+//bool findNum (float tmp) {
+//    for(int i = 0; i < v.size(); i++) {
+//        if(v[i].first == tmp) {
+//            v[i].second++;
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+//
+//int main() {
+//    int n, i;
+//    float tmp;
+//    scanf("%d", &n);
+//    for(i = 0; i < n; i++) {
+//        scanf("%f", &tmp);
+//        if(!findNum(tmp)) {
+//            v.push_back(make_pair(tmp, 1));
+//        }
+//    }
+//    n = 1;
+//    for(i = 0; i < v.size(); i++) {
+//        if(v[i].second > n) {
+//            n = v[i].second;
+//            tmp = v[i].first;
+//        } else if(v[i].second == n && v[i].first < tmp) {
+//            tmp = v[i].first;
+//        }
+//    }
+//    printf("%d\n", tmp);
+//    return 0;
+//}
+
+#include <iostream>
 #include <vector>
 using namespace std;
-vector<pair<long long int, int>> v;
+vector<pair<string, int>> v;
 
-bool findNum (long long int tmp) {
+string smallerNum (string a, string b) {
+    if(a[0] == '-' && b[0] == '-') {
+        if(a.length() > b.length()) { return a; }
+        else if(a.length() < b.length()) { return b; }
+        else {
+            for(int i = 1; i < a.length(); i++) {
+                if(a[i] > b[i]) { return a; }
+                else if(a[i] < b[i]) { return b; }
+            }
+        }
+    } else if(a[0] == '-') {
+        return a;
+    } else if(b[0] == '-') {
+        return b;
+    } else {
+        if(a.length() < b.length()) { return a; }
+        else if(a.length() > b.length()) { return b; }
+        else {
+            for(int i = 0; i < a.length(); i++) {
+                if(a[i] < b[i]) { return a; }
+                else if(a[i] > b[i]) { return b; }
+            }
+        }
+    }
+}
+
+bool findNum (string tmp) {
     for(int i = 0; i < v.size(); i++) {
         if(v[i].first == tmp) {
             v[i].second++;
@@ -14,11 +79,14 @@ bool findNum (long long int tmp) {
 }
 
 int main() {
+    ios_base::sync_with_stdio();
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, i;
-    long long int tmp;
-    scanf("%d", &n);
+    string tmp;
+    cin >> n;
     for(i = 0; i < n; i++) {
-        scanf("%lld", &tmp);
+        cin >> tmp;
         if(!findNum(tmp)) {
             v.push_back(make_pair(tmp, 1));
         }
@@ -26,12 +94,12 @@ int main() {
     n = 1;
     for(i = 0; i < v.size(); i++) {
         if(v[i].second > n) {
+            tmp = v[i].first;
             n = v[i].second;
-            tmp = v[i].first;
-        } else if(v[i].second == n && v[i].first < tmp) {
-            tmp = v[i].first;
+        } else if (v[i].second == n) {
+            tmp = smallerNum(tmp, v[i].first);
         }
     }
-    printf("%d\n", tmp);
+    cout << tmp << '\n';
     return 0;
 }
