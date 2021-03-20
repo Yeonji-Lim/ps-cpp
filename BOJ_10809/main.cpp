5f0a9200ebc66737602ca08532887ddba74f7167
Yeonji-Lim <view7186@naver.com>
2021-03-20 21:15:39 +0900

    Failed - 글자가 중복되는 경우 생각 안함, 처음 등장 위치를 따져야 함

---
+++ BOJ_10809/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+    int a[26], i;
+    memset(a, -1, sizeof(a));
+    char s[100];
+    scanf("%s", s);
+    for(i = 0; s[i] != '\0'; i++) {
+        a[s[i]-97] = i;
+    }
+    for(i = 0; i < 26; i++) {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
