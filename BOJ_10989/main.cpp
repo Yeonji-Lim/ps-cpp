55e57dcec16848602a86a09a1a20f76bda63ddff
Yeonji-Lim <view7186@naver.com>
2021-03-20 16:05:52 +0900

    Solved - 1156KB 2708ms

--- BOJ_10989/main.cpp
+++ BOJ_10989/main.cpp
@@ -45,3 +45,25 @@
 //    countingSort(nums);
 //    return 0;
 //}
+
+/* 2021.3.18 Solved - 1156KB 2708ms */
+#include <cstdio>
+int numList[10001];
+
+int main() {
+    int n, i, tmp, max = 1;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &tmp);
+        numList[tmp]++;
+        if(max < tmp) max = tmp;
+    }
+    for(i = 1; i <= max; i++) {
+        if(numList[i] != 0) {
+            printf("%d\n", i);
+            numList[i]--;
+            i--;
+        }
+    }
+    return 0;
+}
