b95cbc6b5e6137beda4d5b305f5530113b8d74a2
Yeonji-Lim <view7186@naver.com>
2021-03-22 14:23:15 +0900

    범위 잘 못 설정한거 고침

--- BOJ_10820/main.cpp
+++ BOJ_10820/main.cpp
@@ -2,15 +2,15 @@
 
 int main() {
     int n, a[4] = {0};
-    char s[100];
+    char s[101];
     scanf("%d", &n);
     for(int i = 0; i < n; i++) {
         scanf("%[^\n]", s);
         for(int j = 0; s[j] != '\0'; j++) {
             if(s[j] == ' ') { a[3]++; }
             else if(s[j] > 47 && s[j] < 58) { a[2]++; }
-            else if (s[j] > 64 && s[j] < 92) { a[1]++; }
-            else if (s[j] > 96 && s[j] < 124) { a[0]++; }
+            else if (s[j] > 64 && s[j] < 91) { a[1]++; }
+            else if (s[j] > 96 && s[j] < 123) { a[0]++; }
         }
         printf("%d %d %d %d\n", a[0], a[1], a[2], a[3]);
     }
