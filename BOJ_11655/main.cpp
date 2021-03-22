11264958623ca055ca65575e17494c997c434e17
Yeonji-Lim <view7186@naver.com>
2021-03-22 16:18:54 +0900

    Solved - 1116KB 0ms

--- BOJ_11655/main.cpp
+++ BOJ_11655/main.cpp
@@ -1,19 +1,15 @@
+/* 2021.3.22 Solved - 1116KB 0ms */
 #include <cstdio>
 
 int main() {
     char s[101];
-    char c;
     scanf("%[^\n]s", s);
     for(int i = 0; s[i] != '\0'; i++) {
         if(s[i] == ' ' || (s[i] > 47 && s[i] < 58)) { printf("%c", s[i]); }
-        else {
-            c = s[i]+13;
-            if(s[i] > 64 && s[i] < 91 && c > 90) {
-                c = 'A' + (c - 'Z') - 1;
-            } else if(s[i] > 96 && s[i] < 123 && c > 122){
-                c = 'a' + (c - 'z') - 1;
-            }
-            printf("%c", c);
+        else if((s[i] > 64 && s[i] < 91 && s[i]+13 > 90) || (s[i] > 96 && s[i] < 123 && s[i]+13 > 122)) {
+            printf("%c", s[i]-13);
+        } else {
+            printf("%c", s[i]+13);
         }
     }
     printf("\n");
