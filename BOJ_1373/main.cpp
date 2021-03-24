819bf7f6bbef7d91c1bace75c5228b48681cbf9f
Yeonji-Lim <view7186@naver.com>
2021-03-24 22:31:45 +0900

    Solved - 5340KB 68ms

--- BOJ_1373/main.cpp
+++ BOJ_1373/main.cpp
@@ -1,17 +1,36 @@
+/*2021.3.24 Solved - 5340KB 68ms */
 #include <cstdio>
-int binaryToOctal(int a) {
-    int ret = 0, tmp = 1;
-    while(a != 0) {
-        ret += (a%10 + (a%100-a%10)/10*2 + (a%1000-a%100)/100*4)*tmp;
-        tmp *= 10;
-        a /= 1000;
-    }
-    return ret;
-}
+#include <vector>
+using namespace std;
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    printf("%d", binaryToOctal(n));
+    vector<char> n;
+    vector<int> rst;
+    char c;
+    int tmp = 0;
+    scanf("%c", &c);
+    while(c != '\n') {
+        n.push_back(c);
+        c = '*';
+        scanf("%c", &c);
+    }
+    while(!n.empty()) {
+        tmp = n[n.size()-1]-'0';
+        n.pop_back();
+        if(!n.empty()) {
+            tmp += (n[n.size()-1]-'0')*2;
+            n.pop_back();
+            if(!n.empty()) {
+                tmp += (n[n.size()-1]-'0')*4;
+                n.pop_back();
+            }
+        }
+        rst.push_back(tmp);
+        tmp = 0;
+    }
+    while(!rst.empty()) {
+        printf("%d", rst[rst.size()-1]);
+        rst.pop_back();
+    }
     return 0;
 }
