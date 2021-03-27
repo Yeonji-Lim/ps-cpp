c58ab2b9371fc929502b73f7983f3c6ae6e82066
Yeonji-Lim <view7186@naver.com>
2021-03-27 15:14:32 +0900

    .

---
+++ BOJ_2089/cmake-build-debug/CMakeFiles/BOJ_2089.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BOJ_2089"
+  "BOJ_2089.pdb"
+  "CMakeFiles/BOJ_2089.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOJ_2089.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
