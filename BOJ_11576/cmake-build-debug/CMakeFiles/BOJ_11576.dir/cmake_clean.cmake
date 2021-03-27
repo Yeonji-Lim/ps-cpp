cd5b93fac75f9baaf93a332616ef76fe8f6123e7
Yeonji-Lim <view7186@naver.com>
2021-03-27 15:37:53 +0900

    Solved - 1116KB 0ms

---
+++ BOJ_11576/cmake-build-debug/CMakeFiles/BOJ_11576.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BOJ_11576"
+  "BOJ_11576.pdb"
+  "CMakeFiles/BOJ_11576.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOJ_11576.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
