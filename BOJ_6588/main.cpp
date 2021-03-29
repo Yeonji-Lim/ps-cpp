cefa77daf0533a4fc76c3237a9397cffaf305cf3
Yeonji-Lim <view7186@naver.com>
2021-03-29 17:01:57 +0900

    Failed

--- BOJ_6588/main.cpp
+++ BOJ_6588/main.cpp
@@ -1,40 +1,29 @@
 #include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 using namespace std;
-int a, b;
-vector<int> p;
-
-bool isPrime(int n) {
-    for(int i = 2; i < n; i++) {
-        if(n%i == 0) { return false; }
-    }
-    return true;
-}
-
-bool isCorrect(int n) {
-    for(int i = 3; i < n/2; i++) {
-        if(isPrime(i)){
-            p.push_back(i);
-            if(isPrime(n-i)) {
-                p.push_back(n-i);
-                a = i; b = n-i;
-                sort(unique(p.begin(), p.end()), p.end());
-                return true;
-            }
-            sort(unique(p.begin(), p.end()), p.end());
-        }
-    }
-    return false;
-}
 
 int main() {
-    int n = 0;
+    int n = 0, i, j;
+    bool isPrime[1000001];
+
+    for(i = 2; i < 1000001; i++) {
+        isPrime[i] = true;
+    }
+    for(i = 2; i < sqrt(1000000); i++) {
+        for(j = i*2; j < 1000001; j += i) {
+            isPrime[j] = false;
+        }
+    }
+
     scanf("%d", &n);
     while(n != 0) {
-        if(isCorrect(n)) {
-            printf("%d = %d + %d\n", n, a, b);
-        } else {
+        for(i = 3; i < n/2; i++) {
+            if(isPrime[i] && isPrime[n-i]) {
+                printf("%d = %d + %d\n", n, i, n-i);
+                break;
+            }
+        }
+        if(i == n/2) {
             printf("Goldbach's conjecture is wrong.\n");
         }
         scanf("%d", &n);
