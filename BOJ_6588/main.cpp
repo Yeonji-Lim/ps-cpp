7450d0203b19a0ab6c52322f5bce95ffd3a67468
Yeonji-Lim <view7186@naver.com>
2021-03-29 17:25:01 +0900

    Solved - 1968KB 40ms

--- BOJ_6588/main.cpp
+++ BOJ_6588/main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main() {
     int n = 0, i, j;
     bool isPrime[1000001];
-
+    isPrime[1] = false;
     for(i = 2; i < 1000001; i++) {
         isPrime[i] = true;
     }
+
     for(i = 2; i < sqrt(1000000); i++) {
         for(j = i*2; j < 1000001; j += i) {
             isPrime[j] = false;
@@ -17,13 +18,13 @@ int main() {
 
     scanf("%d", &n);
     while(n != 0) {
-        for(i = 3; i < n/2; i++) {
+        for(i = 3; i < n/2 + 1; i += 2) {
             if(isPrime[i] && isPrime[n-i]) {
                 printf("%d = %d + %d\n", n, i, n-i);
                 break;
             }
         }
-        if(i == n/2) {
+        if(i == n/2 + 1) {
             printf("Goldbach's conjecture is wrong.\n");
         }
         scanf("%d", &n);
