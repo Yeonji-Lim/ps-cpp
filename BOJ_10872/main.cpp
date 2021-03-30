24ffadb19bcaa2e0d2dc0cf6eb233a43f848f19f
Yeonji-Lim <view7186@naver.com>
2021-03-30 15:49:53 +0900

    Solved - 1984KB 0ms

--- BOJ_10872/main.cpp
+++ BOJ_10872/main.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int Factorial (int num) {
-    if (num == 0) {
-        return 1;
-    }
-    else return Factorial(num-1) * num;
-}
-
-int main() {
-    int num;
-    cin >> num;
-    cout << Factorial(num) << endl;
-    return 0;
-}
+/* Solved - 1984KB 0ms */
+//#include <iostream>
+//using namespace std;
+//
+//int Factorial (int num) {
+//    if (num == 0) {
+//        return 1;
+//    }
+//    else return Factorial(num-1) * num;
+//}
+//
+//int main() {
+//    int num;
+//    cin >> num;
+//    cout << Factorial(num) << endl;
+//    return 0;
+//}
