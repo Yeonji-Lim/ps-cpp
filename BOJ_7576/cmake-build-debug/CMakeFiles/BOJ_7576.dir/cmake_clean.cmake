97296324469edb24cac89aba3b8685913c429f51
Yeonji-Lim <view7186@naver.com>
2021-04-13 20:13:54 +0900

    시간 초과

---
+++ BOJ_7576/cmake-build-debug/CMakeFiles/BOJ_7576.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BOJ_7576"
+  "BOJ_7576.pdb"
+  "CMakeFiles/BOJ_7576.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOJ_7576.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
