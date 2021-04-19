927903ae712fe4b0ffe2cf5e820cb8c9f296b8c3
Yeonji-Lim <view7186@naver.com>
2021-04-19 19:15:57 +0900

    푸는 중...

---
+++ BOJ_1167/cmake-build-debug/CMakeFiles/BOJ_1167.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BOJ_1167.
+# This may be replaced when dependencies are built.
---
+++ BOJ_1167/main.cpp
@@ -0,0 +1,14 @@
+/* 2021.4.19 */
+#include <cstdio>
+#include <vector>
+#define MAX 100001;
+using namespace std;
+
+int main() {
+    int v, a, b;
+    scanf("%d", &v);
+    for(int i = 1; i <= v; i++) {
+        scanf("%d %d", &a, &b);
+    }
+    return 0;
+}
