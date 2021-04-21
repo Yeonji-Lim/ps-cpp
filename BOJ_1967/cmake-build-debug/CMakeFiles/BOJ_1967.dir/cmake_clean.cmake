f7247856bbd624a5d8c16e9231e791e697ca74de
Yeonji-Lim <view7186@naver.com>
2021-04-21 21:12:29 +0900

    푸는 중...

---
+++ BOJ_1967/cmake-build-debug/CMakeFiles/BOJ_1967.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BOJ_1967"
+  "BOJ_1967.pdb"
+  "CMakeFiles/BOJ_1967.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BOJ_1967.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
