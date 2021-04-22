443aa13c67df08606c4b661fe74236060dc5792d
Yeonji-Lim <view7186@naver.com>
2021-04-22 23:32:51 +0900

    Failed

--- BOJ_1654/main.cpp
+++ BOJ_1654/main.cpp
@@ -36,38 +36,57 @@
 //    return 0;
 //}
 
-/* 2021.4.22 */
+/* 2021.4.22 Failed */
 #include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
+vector<int> wires;
+int k, n;
+long long rst;
+
+void findStartPoint (long long l, long long r) {
+    long long m = (l+r)/2, cnt = 0;
+    if(m == l || m == r) {
+        rst = m;
+        return;
+    }
+    for(int i = 0; i < k; i++) {
+        cnt += wires[i]/m;
+    }
+    if(cnt < n) {
+        findStartPoint(l, m-1);
+    } else {
+        findStartPoint(m+1, r);
+    }
+}
+
+void findResult (long long length) {
+    long long cnt = 0;
+    for(int i = 0; i < k; i++) {
+        cnt += wires[i]/length;
+    }
+    if(cnt >= n) {
+        findResult(length+1);
+    } else {
+        rst = length-1;
+    }
+}
 
 int main() {
-    int k, n, tmp, cnt = 0, length;
+    long long tmp, maxLength = 0;
     scanf("%d %d", &k, &n);
-    vector<int> wires;
     for(int i = 0; i < k; i++) {
-        scanf("%d", &tmp);
+        scanf("%lld", &tmp);
         wires.push_back(tmp);
+        maxLength += tmp;
     }
+    maxLength /= n;
     sort(wires.begin(), wires.end());
-    tmp = n/k;
-    while(cnt < n) {
-        cnt = 0;
-        length = wires[k-1]/tmp;
-        for(int i = 0; i < k; i++) {
-            cnt += wires[i]/length;
-        }
-        tmp++;
-    }
-    while(cnt >= n) {
-        length++;
-        cnt = 0;
-        for(int i = 0; i < k; i++) {
-            cnt += wires[i]/length;
-        }
-    }
-    length--;
-    printf("%d\n", length);
+    tmp = n/k+1;
+    tmp = wires[0]/tmp;
+    findStartPoint(tmp, maxLength);
+    findResult(rst);
+    printf("%lld\n", rst);
     return 0;
 }
