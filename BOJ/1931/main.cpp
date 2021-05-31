ea8bd8a3b625587b1996be1c6b07fda20f114cb8
Yeonji-Lim <view7186@naver.com>
2021-05-31 20:40:54 +0900

    Failed

--- BOJ/1931/main.cpp
+++ BOJ/1931/main.cpp
@@ -1,38 +1,72 @@
-/* 2021.5.29 */
+/* 2021.5.29 Failed - 시간 초과 */
+//#include <cstdio>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//vector<pair<int, int>> v;
+//
+//int max (int a, int b) { return a > b? a: b; }
+//
+//int numOfCon (int tgt) {
+//    int ret = 1, endTime, startTime = tgt;
+//    for(int i = tgt+1; i < v.size(); i++) {
+//        endTime = v[tgt].first + v[tgt].second;
+//        if(v[i].first > endTime) {
+//            ret++;
+//            tgt = i;
+//        }
+//    }
+//    return ret;
+//}
+//
+//int main() {
+//    int n, a, b;
+//    vector<vector<pair<int, int>>> r;
+//    vector<pair<int, int>> t;
+//    scanf("%d", &n);
+//    for(int i = 0; i < n; i++) {
+//        scanf("%d %d", &a, &b);
+//        v.emplace_back(a, b-a);
+//    }
+//    sort(v.begin(), v.end());
+//    a = -1;
+//    for(int i = 0; i < n; i++) {
+//        a = max(a, numOfCon(i));
+//    }
+//    printf("%d\n", a);
+//    return 0;
+//}
+
+/* 2021.5.31 */
 #include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<pair<int, int>> v;
 
-int max (int a, int b) { return a > b? a: b; }
-
-int numOfCon (int tgt) {
-    int ret = 1, endTime, startTime = tgt;
-    for(int i = tgt+1; i < v.size(); i++) {
-        endTime = v[tgt].first + v[tgt].second;
-        if(v[i].first > endTime) {
-            ret++;
-            tgt = i;
-        }
-    }
-    return ret;
+bool compare(pair<int, int> a, pair<int, int> b) {
+    if(a.second == b.second) return a.first < b.first;
+    else return a.second < b.second;
 }
 
 int main() {
-    int n, a, b;
-    vector<vector<pair<int, int>>> r;
-    vector<pair<int, int>> t;
+    int n, a, b, i, rst = 1;
+    vector<pair<int, int>> v;
     scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) {
         scanf("%d %d", &a, &b);
-        v.emplace_back(a, b-a);
+        v.emplace_back(a, b);
     }
-    sort(v.begin(), v.end());
-    a = -1;
-    for(int i = 0; i < n; i++) {
-        a = max(a, numOfCon(i));
+    sort(v.begin(), v.end(), compare);
+    a = v[0].second;
+    i = 1;
+    while(true) {
+        if(i == v.size()) break;
+        if(v[i].first > a) {
+            a = v[i].second;
+            rst++;
+        }
+        i++;
     }
-    printf("%d\n", a);
+    printf("%d", rst);
     return 0;
 }
