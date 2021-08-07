bd35bab575b81b01949eeda5fa5a9ef27f425c90
Yeonji-Lim <view7186@naver.com>
2021-08-07 19:38:50 +0900

    Failed

--- BOJ/1451/main.cpp
+++ BOJ/1451/main.cpp
@@ -1,88 +1,175 @@
+/* 2021.7.17 Failed */
+
+// #include <iostream>
+// #include <climits>
+// #include <vector>
+// using namespace std;
+// int n, m;
+// int map[100][100];
+
+// int max(int a, int b) { return a > b ? a : b; }
+// bool isInMap(int i, int j) { return (i > -1 && i < n && j > -1 && j < m); }
+
+// int squareSum(int si, int sj, int ei, int ej)
+// {
+//     int ret = 0;
+//     if (isInMap(si, sj) && isInMap(ei, ej))
+//     {
+//         for (int i = si; i <= ei; i++)
+//             for (int j = sj; j <= ej; j++)
+//                 ret += map[i][j];
+//     }
+//     else
+//         return -90;
+//     return ret;
+// }
+
+// int main()
+// {
+//     int ans = INT_MIN;
+//     string tmp;
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+//     cin >> n >> m;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> tmp;
+//         int k = 0;
+//         for (int j = 0; j < m; j++)
+//         {
+//             if ('0' <= tmp[k] && tmp[k] <= '9')
+//                 map[i][j] = tmp[j] - 48;
+//             else
+//                 map[i][j] = (-1) * (tmp[++k] - 48);
+//             k++;
+//         }
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             if (i == n - 1 && j == m - 1)
+//                 break;
+//             int fir = squareSum(0, 0, i, j);
+//             int sec, thr;
+//             if (i != n - 1 && j != m - 1)
+//             {
+//                 sec = squareSum(0, j + 1, i, m - 1);
+//                 thr = squareSum(i + 1, 0, n - 1, m - 1);
+//                 ans = max(ans, fir * sec * thr);
+//                 sec = squareSum(0, j + 1, n - 1, m - 1);
+//                 thr = squareSum(i + 1, 0, n - 1, j);
+//                 ans = max(ans, fir * sec * thr);
+//             }
+//             else
+//             {
+//                 int ni = 0;
+//                 int nj = j + 1;
+//                 if (j == m - 1)
+//                 {
+//                     ni = i + 1;
+//                     nj = 0;
+//                 }
+//                 for (int k = 0; k < n - ni - 1; k++)
+//                 {
+//                     sec = squareSum(ni, nj, ni + k, m - 1);
+//                     thr = squareSum(ni + k + 1, nj, n - 1, m - 1);
+//                     ans = max(ans, fir * sec * thr);
+//                 }
+//                 for (int k = 0; k < m - nj - 1; k++)
+//                 {
+//                     sec = squareSum(ni, nj, n - 1, nj + k);
+//                     thr = squareSum(ni, nj + k + 1, n - 1, m - 1);
+//                     ans = max(ans, fir * sec * thr);
+//                 }
+//             }
+//         }
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
+
+/* 2021.8.7 Failed */
 #include <iostream>
-#include <climits>
 #include <vector>
 using namespace std;
-int n, m;
-int map[100][100];
+int n, m, map[101][101], ans = 0;
 
 int max(int a, int b) { return a > b ? a : b; }
-bool isInMap(int i, int j) { return (i > -1 && i < n && j > -1 && j < m); }
 
-int squareSum(int si, int sj, int ei, int ej)
-{
-    int ret = 0;
-    if (isInMap(si, sj) && isInMap(ei, ej))
-    {
-        for (int i = si; i <= ei; i++)
-            for (int j = sj; j <= ej; j++)
-                ret += map[i][j];
+int sepHV(bool h) {
+    int ret = 1, tmp = 0, a = n, b = m;
+    vector<int> tmpV;
+    if (!h) { a = m; b = n; }
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++)
+            if (h) tmp += map[i][j];
+            else tmp += map[j][i];
+        tmpV.push_back(tmp);
+        tmp = 0;
     }
-    else
-        return -90;
+    int fir = 0, sec = 0, thr = 0, t1 = 1, t2 = 2;
+    while (t1 < a - 1) {
+        for (int i = 0; i < t1; i++) fir += tmpV[i];
+        while (t2 < a) {
+            for (int i = t1; i < t2; i++) sec += tmpV[i];
+            for (int i = t2; i < a; i++) thr += tmpV[i];
+            ret = max(fir * sec * thr, ret);
+            sec = 0;
+            thr = 0;
+            t2++;
+        }
+        fir = 0;
+        t2 = ++t1 + 1;
+    }
+    return ret;
+}
+
+int sepNom(int a, int b) {
+    int ret = 0;
+    int part[4] = {0, 0, 0, 0};
+    for(int i = 0; i < a+1; i++) {
+        for(int j = 0; j < b+1; j++)
+            part[0] += map[i][j];
+        for(int j = b+1; j < m; j++)
+            part[1] += map[i][j];
+    }
+    for(int i = a+1; i < n; i++) {
+        for (int j = 0; j < b+1; j++)
+            part[2] += map[i][j];
+        for (int j = b+1; j < m; j++)
+            part[3] += map[i][j];
+    }
+
+    ret = max((part[0]+part[1]) * part[2] * part[3], ret);
+    ret = max((part[0]+part[2]) * part[1] * part[3], ret);
+    ret = max((part[2]+part[3]) * part[0] * part[1], ret);
+    ret = max((part[1]+part[3]) * part[0] * part[2], ret);
     return ret;
 }
 
 int main()
 {
-    int ans = INT_MIN;
-    string tmp;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> tmp;
-        int k = 0;
-        for (int j = 0; j < m; j++)
-        {
-            if ('0' <= tmp[k] && tmp[k] <= '9')
-                map[i][j] = tmp[j] - 48;
-            else
-                map[i][j] = (-1) * (tmp[++k] - 48);
-            k++;
+    string str;
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        for (int j = 0; j < m; j++) {
+            map[i][j] = str[j] - '0';
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (i == n - 1 && j == m - 1)
-                break;
-            int fir = squareSum(0, 0, i, j);
-            int sec, thr;
-            if (i != n - 1 && j != m - 1)
-            {
-                sec = squareSum(0, j + 1, i, m - 1);
-                thr = squareSum(i + 1, 0, n - 1, m - 1);
-                ans = max(ans, fir * sec * thr);
-                sec = squareSum(0, j + 1, n - 1, m - 1);
-                thr = squareSum(i + 1, 0, n - 1, j);
-                ans = max(ans, fir * sec * thr);
-            }
-            else
-            {
-                int ni = 0;
-                int nj = j + 1;
-                if (j == m - 1)
-                {
-                    ni = i + 1;
-                    nj = 0;
-                }
-                for (int k = 0; k < n - ni - 1; k++)
-                {
-                    sec = squareSum(ni, nj, ni + k, m - 1);
-                    thr = squareSum(ni + k + 1, nj, n - 1, m - 1);
-                    ans = max(ans, fir * sec * thr);
-                }
-                for (int k = 0; k < m - nj - 1; k++)
-                {
-                    sec = squareSum(ni, nj, n - 1, nj + k);
-                    thr = squareSum(ni, nj + k + 1, n - 1, m - 1);
-                    ans = max(ans, fir * sec * thr);
-                }
-            }
-        }
+    if (n > 2)
+        ans = max(ans, sepHV(true));
+    if (m > 2)
+        ans = max(ans, sepHV(false));
+    if(n > 1 && m >1) {
+        for(int i = 0; i < n-1; i++) 
+            for(int j = 0; j < m-1; j++) 
+                ans = max(ans, sepNom(i, j));
     }
     cout << ans << endl;
     return 0;
