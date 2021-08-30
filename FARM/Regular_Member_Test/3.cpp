9b13c410020c9437f532792608c17fd36cfddba4
Yeonji-Lim <view7186@naver.com>
2021-08-30 23:52:57 +0900

    Refactoring

--- FARM/Regular_Member_Test/3.cpp
+++ FARM/Regular_Member_Test/3.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 vector<string> split(string str, char delimiter) {
-    vector<string> answer;
+    vector<string> ret;
     stringstream ss(str);
-    string temp;
+    string tmp;
 
-    while (getline(ss, temp, delimiter)) { answer.push_back(temp); }
+    while (getline(ss, tmp, delimiter)) { ret.push_back(tmp); }
 
-    return answer;
+    return ret;
 }
 
 int main() {
