8ffa4729ead7cae74ca8e496983119b9ad996e9f
Yeonji-Lim <view7186@naver.com>
2021-08-30 23:21:40 +0900

    Modify

--- Algorithm/IO/no_end_condition_with_blank.cpp
+++ Algorithm/IO/no_end_condition_with_blank.cpp
@@ -14,5 +14,33 @@ bool chli_to_ili(char chli[], vector<int>& ili) {
             cnt++;
         } else { return false; }
     }
+    ili.push_back(tmp);
     return true;
+}
+
+bool str_to_ili(string str, vector<int> &ili) {
+    int tmp = 0, cnt = 0;
+    for (int i = 0; cnt < 40 && str[i]; i++) {
+        if (str[i] != ' ') {
+            tmp = tmp * 10 + str[i] - '0';
+        }
+        else if (tmp > 0) {
+            ili.push_back(tmp);
+            tmp = 0;
+            cnt++;
+        }
+        else { return false; }
+    }
+    ili.push_back(tmp);
+    return true;
+}
+
+int main() {
+    char chli[40];
+    cin.getline(chli, 40);
+
+    string str;
+    getline(cin, str);
+
+    return 0;
 }
--- FARM/Regular_Member_Test/2.cpp
+++ FARM/Regular_Member_Test/2.cpp
@@ -6,11 +6,11 @@
 #include <vector>
 using namespace std;
 
-bool chli_to_ili(char chli[], vector<int>& ili) {
+bool str_to_ili(string str, vector<int>& ili) {
     int tmp = 0, cnt = 0;
-    for (int i = 0; cnt < 40 && chli[i]; i++) {
-        if (chli[i] != ' ') {
-            tmp = tmp * 10 + chli[i] - '0';
+    for (int i = 0; cnt < 40 && str[i]; i++) {
+        if (str[i] != ' ') {
+            tmp = tmp * 10 + str[i] - '0';
         }
         else if (tmp > 0) {
             ili.push_back(tmp);
@@ -18,20 +18,21 @@ bool chli_to_ili(char chli[], vector<int>& ili) {
             cnt++;
         } else { return false; }
     }
+    ili.push_back(tmp);
     return true;
 }
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     vector<int> nums;
-    char tmp_str[40];
+    string tmp_str;
     int max_num = INT_MIN, max_idx;
 
-    cin.getline(tmp_str, 40);
-    if(chli_to_ili(tmp_str, nums)) {
+    getline(cin, tmp_str);
+    if(str_to_ili(tmp_str, nums)) {
+
         for(int i = 0; i < nums.size(); i++) {
             if(max_num < nums[i]){
                 max_num = nums[i];
@@ -39,9 +40,8 @@ int main()
             }
         }
         nums.erase(nums.begin()+max_idx);
-        for(int i = 0; i < nums.size(); i++) {
-            cout << nums[i] << ' ';
-        }
+
+        for(int i = 0; i < nums.size(); i++) { cout << nums[i] << ' '; }
         cout << endl;
     }
     return 0;
