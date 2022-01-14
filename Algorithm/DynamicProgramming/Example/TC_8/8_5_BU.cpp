f8e7b74601cd9c1a8914be81874b423d2bc231e1
Yeonji-Lim <view7186@naver.com>
2022-01-14 23:55:45 +0900

    TC_DP 완료

--- Algorithm/DynamicProgramming/Example/TC_8/8_5_BU.cpp
+++ Algorithm/DynamicProgramming/Example/TC_8/8_5_BU.cpp
@@ -1,14 +1,56 @@
+// #include <iostream>
+// using namespace std;
+
+// int N, M;
+// int mon[100], DP[10001] = {0};
+
+// int max(int a, int b) { return a < b? a: b; }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL); cout.tie(NULL);
+
+//     cin >> N >> M;
+
+//     for(int i = 0; i < N; i++) {
+//         cin >> mon[i];
+//     }
+
+//     for(int i = 1; i <= M; i++) {
+//         if(i < mon[0]) DP[i] = -1;
+//         else {
+//             for(int k = 0; k < N; k++) {
+//                 if(i < mon[k]) {
+//                     DP[i] = -1;
+//                     continue;
+//                 }
+//                 if(DP[i-mon[k]] == -1) continue;
+//                 if(DP[i] != 0) DP[i] = min(DP[i], 1 + DP[i-mon[k]]);
+//                 else DP[i] = 1 + DP[i-mon[k]];
+//             }
+//             if(DP[i] == 0) DP[i] = -1;
+//         }
+//     }
+
+//     cout << DP[M] << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int N, M;
-int mon[100], DP[10001] = {0};
+int mon[100], DP[10001];
 
-int max(int a, int b) { return a < b? a: b; }
+int min(int a, int b) { return a < b? a: b; }
 
 int main() {
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+
+    fill_n(DP, 10001, 10001);
 
     cin >> N >> M;
 
@@ -16,19 +58,18 @@ int main() {
         cin >> mon[i];
     }
 
+    DP[0] = 0;
+
     for(int i = 1; i <= M; i++) {
         if(i < mon[0]) DP[i] = -1;
         else {
-            for(int k = 0; k < N; k++) {
-                if(i < mon[k]) {
-                    DP[i] = -1;
-                    continue;
-                }
-                if(DP[i-mon[k]] == -1) continue;
-                if(DP[i] != 0) DP[i] = min(DP[i], 1 + DP[i-mon[k]]);
-                else DP[i] = 1 + DP[i-mon[k]];
+            for(int j = 0; j < N; j++){
+                int tmp = i-mon[j];
+                if(tmp > -1) {
+                    if(DP[tmp] != -1) DP[i] = min(DP[i], DP[tmp]+1);
+                } else break;
             }
-            if(DP[i] == 0) DP[i] = -1;
+            if(DP[i] == 10001) DP[i] = -1;
         }
     }
 
--- Algorithm/DynamicProgramming/Example/TC_8/8_5_TD.cpp
+++ Algorithm/DynamicProgramming/Example/TC_8/8_5_TD.cpp
@@ -1,25 +1,62 @@
+// #include <iostream>
+// using namespace std;
+
+// int N, M;
+// int mon[100], DP[10001] = {10001};
+
+// int min(int a, int b) { return a < b? a: b;}
+
+// int find(int cur) {
+    
+//     if(cur < mon[0]) DP[cur] = -1;
+//     else if(cur == mon[0]) DP[cur] = 0;
+//     else {
+//         for(int i = 0; i < N; i++) {
+//             if(DP[cur-mon[i]] == -1) continue;
+//             if(DP[cur] != 0) DP[cur] = min(DP[cur-mon[i]]+1, DP[cur]);
+//             else DP[cur] = DP[cur-mon[i]]+1;
+//         }
+
+//         if(DP[cur] == 0) DP[cur] = -1;
+//     }
+    
+//     return DP[cur];
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL); cout.tie(NULL);
+
+//     cin >> N >> M;
+
+//     for(int i = 0; i < N; i++) {
+//         cin >> mon[i];
+//     }
+
+//     cout << find(M) << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int N, M;
-int mon[100], DP[10001] = {10001};
+int mon[100], DP[10001];
 
-int min(int a, int b) { return a < b? a: b;}
+int min(int a, int b) { return a < b? a: b; }
 
 int find(int cur) {
-    
-    if(cur < mon[0]) DP[cur] = -1;
-    else if(cur == mon[0]) DP[cur] = 0;
-    else {
-        for(int i = 0; i < N; i++) {
-            if(DP[cur-mon[i]] == -1) continue;
-            if(DP[cur] != 0) DP[cur] = min(DP[cur-mon[i]]+1, DP[cur]);
-            else DP[cur] = DP[cur-mon[i]]+1;
+    if(DP[cur] == 10001) {
+        if(cur < mon[0]) DP[cur] = -1;
+        for(int j = 0; j < N; j++) {
+            int tmp = cur - mon[j];
+            if(tmp >=0) {
+                int DP_tmp = find(tmp);
+                if(DP_tmp != -1) DP[cur] = min(DP[cur], DP_tmp+1);
+            } else break;
         }
-
-        if(DP[cur] == 0) DP[cur] = -1;
     }
-    
     return DP[cur];
 }
 
@@ -27,11 +64,15 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
+    fill_n(DP, 10001, 10001);
+
     cin >> N >> M;
 
     for(int i = 0; i < N; i++) {
         cin >> mon[i];
     }
+
+    DP[0] = 0;
 
     cout << find(M) << endl;
 
