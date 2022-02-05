10a4a47942270cdd4ca099e25eca197cf1c83db0
Yeonji-Lim <view7186@naver.com>
2022-02-05 17:12:47 +0900

    union을 처음부터 하는게 아니라 나중에 간선 하나씩 확인할때 하는거임

--- Algorithm/Graph/ShortestPath/Kruskal/Example/TC_10/10_3.cpp
+++ Algorithm/Graph/ShortestPath/Kruskal/Example/TC_10/10_3.cpp
@@ -1,3 +1,74 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// #include <tuple>
+// using namespace std;
+
+// int N, M, rst = 0;
+// vector<int> parent;
+// vector<tuple<int, int, int>> sp_tr_1;
+// vector<tuple<int, int, int>> sp_tr_2;
+// vector<tuple<int, int, int>> v;
+
+// bool cmpF(tuple<int, int, int> &x, tuple<int, int, int> &y) {
+//     return get<2>(x) < get<2>(y);
+// }
+
+// int find_parent(int x) {
+//     if(parent[x] != x) {
+//         parent[x] = find_parent(parent[x]);
+//     }
+//     return x;
+// }
+
+// void union_parent(int x, int y) {
+//     x = find_parent(x);
+//     y = find_parent(y);
+
+//     if(x < y)
+//         parent[y] = x;
+//     else
+//         parent[x] = y;
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL); cout.tie(NULL);
+
+//     cin >> N >> M;
+
+//     for(int x = 0; x <= N; x++) {
+//         parent.push_back(x);
+//     }
+
+//     int i, j, c;
+//     while(M-- != 0) {
+//         cin >> i >> j >> c;
+//         union_parent(i, j);
+//         v.emplace_back(i, j, c);
+//     }
+
+//     sort(v.begin(), v.end(), cmpF);
+
+//     c = get<0>(v[0]);
+//     c = find_parent(c);
+
+//     for(int x = 0; x <= v.size(); x++) {
+//         i = find_parent(get<0>(v[x]));
+//         j = find_parent(get<1>(v[x]));
+
+//         if (i != j) {
+//             if(i == c) sp_tr_1.push_back(v[x]);
+//             else sp_tr_2.push_back(v[x]);
+//             rst += get<2>(v[x]);
+//         }
+//     }
+
+//     cout << rst << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -5,16 +76,15 @@
 using namespace std;
 
 int N, M, rst = 0;
-vector<int> parent;
-vector<tuple<int, int, int>> sp_tr_1;
-vector<tuple<int, int, int>> sp_tr_2;
-vector<tuple<int, int, int>> v;
+int parent[100000];
+vector<tuple<int, int, int>> edges;
 
-bool cmpF(tuple<int, int, int> &x, tuple<int, int, int> &y) {
+bool cmp(tuple<int, int, int> &x, tuple<int, int, int> &y) {
     return get<2>(x) < get<2>(y);
 }
 
-int find_parent(int x) {
+    int find_parent(int x)
+{
     if(parent[x] != x) {
         parent[x] = find_parent(parent[x]);
     }
@@ -29,6 +99,7 @@ void union_parent(int x, int y) {
         parent[y] = x;
     else
         parent[x] = y;
+
 }
 
 int main() {
@@ -37,33 +108,31 @@ int main() {
 
     cin >> N >> M;
 
-    for(int x = 0; x <= N; x++) {
-        parent.push_back(x);
+    for(int x = 1; x <= N; x++) {
+        parent[x] = x;
     }
 
     int i, j, c;
     while(M-- != 0) {
         cin >> i >> j >> c;
+        edges.emplace_back(i, j, c);
         union_parent(i, j);
-        v.emplace_back(i, j, c);
     }
 
-    sort(v.begin(), v.end(), cmpF);
+    sort(edges.begin(), edges.end(), cmp);
 
-    c = get<0>(v[0]);
-    c = find_parent(c);
+    for(int x = 0; x < edges.size(); x++) {
+        i = get<0>(edges[x]);
+        j = get<1>(edges[x]);
 
-    for(int x = 0; x <= v.size(); x++) {
-        i = find_parent(get<0>(v[x]));
-        j = find_parent(get<1>(v[x]));
-
-        if (i != j) {
-            if(i == c) sp_tr_1.push_back(v[x]);
-            else sp_tr_2.push_back(v[x]);
-            rst += get<2>(v[x]);
+        if(find_parent(i) != find_parent(j)) {
+            rst += get<2>(edges[x]);
+            c = get<2>(edges[x]);
         }
     }
 
+    rst -= c;
+    
     cout << rst << endl;
 
     return 0;
