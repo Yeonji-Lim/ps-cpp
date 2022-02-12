7a6e3ca33fb7b58ceed129f5957f347b98585bc2
Yeonji-Lim <view7186@naver.com>
2022-02-12 20:26:36 +0900

    Solved - 2156KB 4ms, 연속되는 걸 세야하는데 전체에서 개수 많은 걸 세버렸던 것

--- BOJ/3085/3085.cpp
+++ BOJ/3085/3085.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-int N, rst = 0, candy[4] = {0};
+int N, rst = 0;
 vector<vector<int>> box;
 vector<vector<int>> tmpV;
 int dij[4][2] = { {-1, 0}, {0, -1}, {0, 1}, {1, 0} };
@@ -11,30 +11,31 @@ bool isInBox(int i, int j) { return i >= 0 && i < N && j >= 0 && j < N; }
 int max(int x, int y) { return x > y? x: y; }
 
 void findMaxCandy(vector<vector<int>> &v, int i, int j) {
+    int ret = 1, cnt = 1;
     if(i == -1) {
-        for(int x = 0; x < N; x++) {
-            switch(v[x][j]){
-                case 'C': candy[0]++; break;
-                case 'P': candy[1]++; break;
-                case 'Z': candy[2]++; break;
-                case 'Y': candy[3]++; break;
+        char pre = v[0][j];
+        for(int x = 1; x < N; x++) {
+            if(pre == v[x][j]) {
+                cnt++;
+            } else {
+                pre = v[x][j];
+                cnt = 1;
             }
+            ret = max(ret, cnt);
         }
-        sort(candy, candy+4);
-        v[N][j] = candy[3];
-        fill_n(candy, 4, 0);
+        v[N][j] = ret;
     } else {
-        for(int x = 0; x < N; x++) {
-            switch(v[i][x]){
-                case 'C': candy[0]++; break;
-                case 'P': candy[1]++; break;
-                case 'Z': candy[2]++; break;
-                case 'Y': candy[3]++; break;
+        char pre = v[i][0];
+        for(int x = 1; x < N; x++) {
+            if(pre == v[i][x]) {
+                cnt++;
+            } else {
+                pre = v[i][x];
+                cnt = 1;
             }
+            ret = max(ret, cnt);
         }
-        sort(candy, candy+4);
-        v[i][N] = candy[3];
-        fill_n(candy, 4, 0);
+        v[i][N] = ret;
     }
 }
 
@@ -42,14 +43,16 @@ int switchCandy(int i, int j, int ni, int nj) {
     char tmp = tmpV[i][j];
     tmpV[i][j] = tmpV[ni][nj];
     tmpV[ni][nj] = tmp;
-    if(i == ni) { //좌우로 변경
+    if(i == ni) {
+        findMaxCandy(tmpV, i, -1);
         findMaxCandy(tmpV, -1, j);
         findMaxCandy(tmpV, -1, nj);
-        return max(max(box[N][j], tmpV[N][j]), max(box[N][nj], tmpV[N][nj]));
+        return max(tmpV[i][N], max(tmpV[N][j], tmpV[N][nj]));
     } else {
+        findMaxCandy(tmpV, -1, j);
         findMaxCandy(tmpV, i, -1);
         findMaxCandy(tmpV, ni, -1);
-        return max(max(box[i][N], tmpV[i][N]), max(box[ni][N], tmpV[ni][N]));
+        return max(tmpV[N][j], max(tmpV[i][N], tmpV[ni][N]));
     }
 }
 
@@ -86,6 +89,7 @@ int main() {
     for(int i = 0; i < N; i++) {
         findMaxCandy(box, i, -1);
         findMaxCandy(box, -1, i);
+        rst = max(rst, max(box[i][N], box[N][i]));
     }
 
     for(int i = 0; i < N; i++) {
