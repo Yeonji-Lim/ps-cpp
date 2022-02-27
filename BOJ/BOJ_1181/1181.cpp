62d3cdba6d78a884b5f63116f7e00f3686f8aac0
Yeonji-Lim <57888020+Yeonji-Lim@users.noreply.github.com>
2022-02-27 22:55:59 +0900

    Solving

---
+++ BOJ/BOJ_1181/1181.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+vector<string> v;
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N;
+    
+    v.resize(N, "0");
+    
+    for(int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < N; i++) {
+        cout << v[i] << endl;
+    }
+     
+    return 0;
+}
