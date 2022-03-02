6c798e39c1f7f9406989d8463b6e55499b43b532
Yeonji-Lim <view7186@naver.com>
2022-03-02 20:52:54 +0900

    Solved

--- Algorithm/Greedy/Example/TC_11/11_4.cpp
+++ Algorithm/Greedy/Example/TC_11/11_4.cpp
@@ -1,3 +1,45 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// using namespace std;
+
+// int N;
+// vector<int> coins;
+
+// bool isPossible(int money, vector<int> v) {
+//     if(find(v.begin(), v.end(), money) != v.end()) return true;
+//     for(int i = 0; i < v.size(); i++) {
+//         vector<int> tmp(v.size(), 0);
+//         copy(v.begin(), v.end(), tmp.begin());
+//         tmp.erase(tmp.begin()+i);
+//         if(isPossible(money-v[i], tmp)) return true;
+//     }
+//     return false;
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL); cout.tie(NULL);
+
+//     cin >> N;
+//     coins.resize(N, 0);
+//     int sum = 0, i;
+//     for(i = 0; i < N; i++) { 
+//         cin >> coins[i]; 
+//         sum += coins[i];
+//     }
+
+//     sort(coins.begin(), coins.end());
+
+//     for(i = 1; i <= sum+1; i++) {
+//         if(!isPossible(i, coins)) break;
+//     }
+
+//     cout << i << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -5,37 +47,24 @@ using namespace std;
 
 int N;
 vector<int> coins;
-
-bool isPossible(int money, vector<int> v) {
-    if(find(v.begin(), v.end(), money) != v.end()) return true;
-    for(int i = 0; i < v.size(); i++) {
-        vector<int> tmp(v.size(), 0);
-        copy(v.begin(), v.end(), tmp.begin());
-        tmp.erase(tmp.begin()+i);
-        if(isPossible(money-v[i], tmp)) return true;
-    }
-    return false;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> N;
     coins.resize(N, 0);
-    int sum = 0, i;
-    for(i = 0; i < N; i++) { 
-        cin >> coins[i]; 
-        sum += coins[i];
+    for(int i = 0; i < N; i++) {
+        cin >> coins[i];
     }
-
     sort(coins.begin(), coins.end());
-
-    for(i = 1; i <= sum+1; i++) {
-        if(!isPossible(i, coins)) break;
+    int target = 1;
+    for(int i = 0; i < N; i++) {
+        if(target < coins[i])
+            break;
+        target += coins[i];
     }
 
-    cout << i << endl;
+    cout << target << endl;
 
     return 0;
 }
