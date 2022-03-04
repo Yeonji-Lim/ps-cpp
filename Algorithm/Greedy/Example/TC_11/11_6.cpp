b115d90e25a0de323f22c72aef46788f68a2c713
Yeonji-Lim <view7186@naver.com>
2022-03-04 18:20:41 +0900

    Solved, 풀이 완전히 이해

--- Algorithm/Greedy/Example/TC_11/11_6.cpp
+++ Algorithm/Greedy/Example/TC_11/11_6.cpp
@@ -37,6 +37,39 @@
 //     if(cur != -1) ++cur;
 //     cout << cur << endl;
 //     return 0;
-// } 
+// }
 
-//위와 같이 하면 시간초과..
+// 위와 같이 하면 시간초과..
+// https://programmers.co.kr/learn/courses/30/lessons/42891
+
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool idxCmp (pair<int, int> x, pair<int, int> y) { return x.second < y.second; }
+
+int solution(vector<int> food_times, long long k) {
+    int size = food_times.size(), prev = 0, i;
+    long long spend = 0;
+    
+    vector<pair<int, int>> pq;
+    for(i = 0; i < size; i++) {
+        pq.emplace_back(food_times[i], i+1);
+        spend += food_times[i];
+    }
+    
+    if(spend <= k) return -1;
+    
+    sort(pq.begin(), pq.end());
+    for(i = 0; i < pq.size(); i++, size--) {
+        spend = (long long)(pq[i].first - prev) * size;
+        if(spend > k) break;
+        k -= spend;
+        prev = pq[i].first;
+    }
+    
+    sort(pq.begin()+i, pq.end(), idxCmp);
+    return pq[k%size+i].second;
+}
