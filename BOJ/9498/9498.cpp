86b891b76da0ab8270f1065495a0e7b0c1b23066
Yeonji-Lim <view7186@naver.com>
2022-03-06 23:19:54 +0900

    Solved - 2020KB

---
+++ BOJ/9498/9498.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void main() {
+    int N;
+    cin>>N;
+    if(N>=90) cout<<'A';
+    else if(N>=80) cout<<'B';
+    else if(N>=70) cout<<'C';
+    else if(N>=60) cout<<'D';
+    else cout<<'F';
+}
