d868cc1b498dff1344e271a5420cf68d06da5807
Yeonji-Lim <view7186@naver.com>
2022-03-09 16:54:11 +0900

    Failed - 숫자가 하나도 없는 경우에 대한 예외처리를 안함

---
+++ Algorithm/Implement/Example/TC_12/12_8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool isNum(char t) { return t >= '0' && t <= '9'; }
+bool cmp(char x, char y) {
+    if(isNum(x)) {
+        if(isNum(y)) return x < y;
+        else return false;
+    } else if(isNum(y)) return true;
+    else return x < y;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    string s = "", tmp;
+    int num = 0;
+    cin >> tmp;
+    for(int i = 0; i < tmp.length(); i++) {
+        if(isNum(tmp[i])) num += tmp[i]-'0';
+        else s+=tmp[i];
+    }
+    sort(s.begin(), s.end());
+    s += to_string(num);
+    cout << s << "\n";
+    return 0;
+}
