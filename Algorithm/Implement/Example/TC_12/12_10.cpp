63d39538d79d848a9a5a2eac27a2b496cae27a5b
Yeonji-Lim <view7186@naver.com>
2022-03-16 17:26:26 +0900

    Failed - 문자열은 앞에서 부터 잘라야한다는 규칙을 간과함

---
+++ Algorithm/Implement/Example/TC_12/12_10.cpp
@@ -0,0 +1,38 @@
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// int min(int a, int b) { return a < b? a: b; }
+
+// int solution(string s) {
+//     int answer, len = s.length(), cnt;
+//     string tmp, comps;
+    
+//     answer = len;
+//     for(int u = 1; u <= len/2; u++) {
+//         cnt = 1;
+//         comps = "";
+//         for(int start = 0; start < u; start++) {
+//             tmp = s.substr(start, u);
+//             for(int i = start+u; i <= len-u; i += u) {
+//                 if(s.substr(i, u) == tmp) { cnt++; }
+//                 else {
+//                     if(cnt != 1) {
+//                         comps += to_string(cnt);
+//                         cnt = 1;
+//                     }
+//                     comps += tmp;
+//                     tmp = s.substr(i, u);
+//                 }
+//             }
+//         }
+//         if(cnt != 1) comps += to_string(cnt);
+//         comps += tmp;
+//         answer = min(comps.length(), answer);
+//     }
+    
+//     return answer;
+// }
+
+// -> "문자열은 제일 앞에서 부터 잘라야 한다" : 예시에 나왔다. 예시까지 꼼꼼히 읽자
