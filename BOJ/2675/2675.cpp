4ce1994784942404720130777179d15ae6dfaee0
Yeonji-Lim <57888020+Yeonji-Lim@users.noreply.github.com>
2022-03-20 23:37:44 +0900

    .

---
+++ BOJ/2675/2675.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N, M;
+    string str; 
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> M >> str;
+        for(int j=0; j<str.length(); j++) {
+            for(int k=0; k<M; k++) {
+                cout << str[i];
+            }
+        }
+        cout << endl;
+    }     
+    return 0;
+} 
