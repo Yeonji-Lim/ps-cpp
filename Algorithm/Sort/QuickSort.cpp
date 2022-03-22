62312f9cfb8ce003206f8d41a46bd9c8ca22dcf8
Yeonji-Lim <57888020+Yeonji-Lim@users.noreply.github.com>
2022-03-22 18:07:36 +0900

    sort

--- Algorithm/Sort/QuickSort.cpp
+++ Algorithm/Sort/QuickSort.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+void Sort(vector<int> V, vector<int> &L, vector<int> &R, int p) {
+    int i = 0, j = 0, tmp;
+    while(i < L.size() && j < R.size()) {
+        while(i<L.size() && p>L[i]) i++;
+        while(j<R.size() && p<=R[j]) j++;
+        if(i == L.size || j == R.size) break;
+        tmp = L[i];
+        L[i] = R[j];
+        R[j] = tmp;
+        
+        i++;
+        j++;
+    }
+    while(i<L.size()) {
+        if(p<=L[i]) R.push_back(L[i]);
+        i++;
+    }
+    while(j<R.size()) {
+        if(p>R[j]) L.push_back(R[j]);
+        j++;
+    }
+}
+
+vector<int> Merge(vector<int> L, vector<int> R, int p) {
+    vector<int> ret(L.size()+R.size()+1, 0);
+    copy(L.begin(), L.end(), ret.begin())
+    ret.push_back(p);
+    copy(R.begin(), R.end(), ret.begin()+L.size()+1);
+    return ret;
+}
 
 int main() {
     srand((unsigned int)time(NULL));
