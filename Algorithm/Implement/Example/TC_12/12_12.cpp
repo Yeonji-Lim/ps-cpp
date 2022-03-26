2391a7a40582784962821a9c28b10735712de4b1
Yeonji-Lim <view7186@naver.com>
2022-03-27 00:14:22 +0900

    14.4/100

--- Algorithm/Implement/Example/TC_12/12_12.cpp
+++ Algorithm/Implement/Example/TC_12/12_12.cpp
@@ -6,49 +6,55 @@ using namespace std;
 typedef vector<vector<int>> vvi;
 typedef vector<int> vi;
 
-bool addCheck(vvi ans, vi item)
-{
+bool addCheck(vvi ans, vi item) {
     int x = item[0], y = item[1];
-    if (item[2] == 0)
-    { //기둥
-        if (y == 0 || find(ans.begin(), ans.end(), vi{x, y - 1, 0}) != ans.end() || find(ans.begin(), ans.end(), vi{x, y, 1}) != ans.end() || find(ans.begin(), ans.end(), vi{x - 1, y, 1}) != ans.end())
-            return true;
+    if(item[2] == 0) { //기둥
+        return y == 0
+            || find(ans.begin(), ans.end(), vi{x, y-1, 0}) != ans.end()
+            || find(ans.begin(), ans.end(), vi{x, y, 1}) != ans.end()
+            || find(ans.begin(), ans.end(), vi{x-1, y, 1}) != ans.end();
+    } else { //보
+        return find(ans.begin(), ans.end(), vi{x, y-1, 0}) != ans.end()
+            || find(ans.begin(), ans.end(), vi{x+1, y-1, 0}) != ans.end()
+            || (find(ans.begin(), ans.end(), vi{x-1, y, 1}) != ans.end()
+            && find(ans.begin(), ans.end(), vi{x+1, y, 1}) != ans.end());
     }
-    else
-    { //보
-        if (find(ans.begin(), ans.end(), vi{x, y - 1, 0}) != ans.end() || find(ans.begin(), ans.end(), vi{x + 1, y - 1, 0}) != ans.end() || find(ans.begin(), ans.end(), vi{x - 1, y, 1}) != ans.end() || find(ans.begin(), ans.end(), vi{x + 1, y, 1}) != ans.end())
-            return true;
-    }
-    return false;
 }
 
-bool delCheck(vvi ans, vi item)
-{
-    return false;
+bool delCheck(vvi ans, vi item) {
+    int x = item[0], y = item[1];
+    if(item[2] == 0) { //기둥
+        ans.erase(find(ans.begin(), ans.end(), vi{x, y, 0}));
+        return addCheck(ans, vi{x, y+1, 0})
+            && addCheck(ans, vi{x, y+1, 1})
+            && addCheck(ans, vi{x-1, y+1, 1});
+    } else { //보
+        ans.erase(find(ans.begin(), ans.end(), vi{x, y, 1}));
+        return addCheck(ans, vi{x, y, 0})
+            && addCheck(ans, vi{x+1, y, 0})
+            && addCheck(ans, vi{x-1, y, 1})
+            && addCheck(ans, vi{x+1, y, 1});
+    }
 }
 
-bool compare(vi a, vi b)
-{
-    if (a[0] == b[0])
-    {
-        if (a[1] == b[1])
+bool compare(vi a, vi b) {
+    if(a[0] == b[0]) {
+        if(a[1] == b[1])
             return a[2] < b[2];
         return a[1] < b[1];
     }
     return a[0] < b[0];
 }
 
-vvi solution(int n, vvi build_frame)
-{
+vvi solution(int n, vvi build_frame) {
     vvi ans;
     vi item, target;
-    for (int i = 0; i < build_frame.size(); i++)
-    {
+    for(int i = 0; i < build_frame.size(); i++) {
         item = build_frame[i];
         target = {item[0], item[1], item[2]};
-        if (item[3] == 1 && addCheck(ans, item))
+        if(item[3] == 1 && addCheck(ans, item))
             ans.push_back(target);
-        else if (item[3] == 0 && delCheck(ans, item))
+        else if(item[3] == 0 && delCheck(ans, item))
             ans.erase(find(ans.begin(), ans.end(), target));
     }
     sort(ans.begin(), ans.end(), compare);
