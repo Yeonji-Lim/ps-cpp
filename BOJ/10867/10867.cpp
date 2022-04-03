466b5a87b17a07c51a2f507a1ecb2be1f72416ed
Yeonji-Lim <view7186@naver.com>
2022-04-03 21:18:41 +0900

    Solved - 2412KB 12ms, use STL

--- BOJ/10867/10867.cpp
+++ BOJ/10867/10867.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -8,6 +9,22 @@ int main(){
     
     int N;
     cin >> N;
+
+    vector<int> A(N, 0);
+    for(int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+
+    int tmp = A[0];
+    cout << A[0] << " ";
+    for(int i = 1; i < N; i++) {
+        if(A[i] != tmp) {
+            tmp = A[i];
+            cout << A[i] << " ";
+        }
+    }
+    cout << endl;
     
     return 0;
 }
