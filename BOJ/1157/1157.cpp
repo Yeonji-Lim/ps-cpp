41abc456fbfbd8a398b4f64536b6009fd8771b19
Yeonji-Lim <57888020+Yeonji-Lim@users.noreply.github.com>
2022-04-10 23:14:00 +0900

    solving

---
+++ BOJ/1157/1157.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+     
+    int A[26], max, maxIdx = 0;
+    string str;
+    cin >> str;
+    for(int i =0; i<str.length(); i++){
+        if(str[i] >= 'a') A[str[i]-'a']++;
+        else A[str[i]-'A']++;
+    }
+    max = A[0];
+    for(int i=1; i < 26; i++){
+        if(A[i] > max) {
+            max = A[i];
+            maxIdx = i;
+        }
+    }
+    char c = 'A'+maxIdx;
+    cout << c << endl;
+    return 0;
+}
