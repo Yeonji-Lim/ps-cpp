ef5c4ed6127d09d6233d60acaecc01c7edb64357
Yeonji-Lim <view7186@naver.com>
2022-04-23 23:57:27 +0900

    .

---
+++ BOJ/2775/2775.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int t, k, n;
+    cin >> t;
+    while(t-- != 0) {
+        cin >> k >> n;
+        
+    }
+    
+    return 0;
+}
