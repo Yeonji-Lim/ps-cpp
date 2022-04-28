4fdde7eddc9df1317e9300235b31d2d09f14d284
Yeonji-Lim <view7186@naver.com>
2022-04-29 00:02:07 +0900

    명심 : PS에서의 메모리의 기준을 알자

--- BOJ/18352/18352.cpp
+++ BOJ/18352/18352.cpp
@@ -1,53 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<bool>> graph;
-vector<int> dist;
-stack<int> st;
-queue<int> q;
+vector<int> graph[300001];
+vector<int> dist(300001, -1);
 int n, m, k, x;
-
-// 이 문제는 bfs로 해결해야 한다.
-void dfs(int d) {
-    if(st.empty()) return;
-    int cur = st.top();
-    for(int j = 1; j < n+1; j++) {
-        if(graph[cur][j]) {
-            dist[j] = min(dist[j], d+1);
-            st.push(j);
-            dfs(d+1);
-        }
-    }
-    st.pop();
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
     cin >> n >> m >> k >> x;
-    graph.resize(n+1, vector<bool>(n+1, false));
-    dist.resize(n+1, 1000000);
     int i, j;
     for(int p = 0; p < m; p++) {
         cin >> i >> j;
-        graph[i][j] = true;
+        graph[i].push_back(j);
     }
+    dist[x] = 0;
 
-    // st.push(x);
-    // dfs(0);
-
-    int d = 1;
+    queue<int> q;
     q.push(x);
     while(!q.empty()) {
         int cur = q.front();
         q.pop();
-        for(j = 1; j < n+1; j++) {
-            if(graph[cur][j]){
-                dist[j] = min(dist[j], d);
+        for(i = 0; i < graph[cur].size(); i++) {
+            j = graph[cur][i];
+            if (dist[j] == -1) {
+                dist[j] = dist[cur]+1;
                 q.push(j);
             }
         }
-        d++;
     }
     
     bool find = false;
