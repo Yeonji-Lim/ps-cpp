ce1446bdddc3394f3ed7a9f99486671d30f210c2
Yeonji-Lim <view7186@naver.com>
2022-05-16 01:31:04 +0900

    2024KB 0ms

--- BOJ/5622/5622.cpp
+++ BOJ/5622/5622.cpp
@@ -6,14 +6,10 @@ int main() {
     cin >> word;
     int ans = 0;
     for(int i = 0; i < word.length(); i++){
-        if(word[i] < 'D') ans += 3;
-        else if(word[i] < 'G') ans += 4;
-        else if(word[i] < 'J') ans += 5;
-        else if(word[i] < 'M') ans += 6;
-        else if(word[i] < 'P') ans += 7;
-        else if(word[i] < 'T') ans += 8;
-        else if(word[i] < 'W') ans += 9;
-        else ans += 10;
+        if(word[i] < 'D') ans += 3; else if(word[i] < 'G') ans += 4;
+        else if(word[i] < 'J') ans += 5; else if(word[i] < 'M') ans += 6;
+        else if(word[i] < 'P') ans += 7; else if(word[i] < 'T') ans += 8;
+        else if(word[i] < 'W') ans += 9; else ans += 10;
     } 
     cout << ans << endl;
 }
