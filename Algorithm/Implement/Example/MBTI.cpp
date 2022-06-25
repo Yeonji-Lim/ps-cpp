70b42bb104933b95ceedb6c70cb44b34fa5452ef
Yeonji-Lim <view7186@naver.com>
2022-06-25 23:23:14 +0900

    모든 짝이 될 수 있는 경우의 수 저장

--- Algorithm/Implement/Example/MBTI.cpp
+++ Algorithm/Implement/Example/MBTI.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 vector<int> cost(16);
 vector<vector<int>> pairs;
-vector<bool> mbti(4);
 
 vector<bool> toMbti(int idx) {
     vector<bool> ret;
@@ -40,48 +39,81 @@ bool isOverlap(vector<vector<int>> vs, vector<int> target) {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int T;
-    for(int t = 0; t < 16; t++) {
-        vector<bool> test = toMbti(t);
-        for(int i = 0; i < (int)test.size(); i++) {
-            if(test[i]) cout << "1 ";
-            else cout << "0 ";
+
+    // 미리 n = 1일 때 나올 수 있는 모든 조합을 저장한다. 
+    // 이는 테스트 케이스에 대해 독립적이다.
+    for(int i = 0; i < 8; i++) {
+        pairs.push_back({0, i, 15-i}); // 2개씩 짝
+
+        vector<bool> cur = toMbti(i);
+        for(int j = 0; j < 4; j++) { // 4개씩 짝
+            vector<int> item = {0, i};
+            vector<bool> mbti = cur;
+            mbti[j] = !mbti[j];
+            for(int k = 0; k < 4; k++) {
+                if(k == j) continue;
+                mbti[k] = !mbti[k];
+                item.push_back(toIdx(mbti));
+                mbti[k] = !mbti[k];
+            }
+            pairs.push_back(item);
         }
-        cout << endl;
-        cout << toIdx(test) << endl;
     }
-    
-    // cin >> T;
-    // for(int t = 1; t <= T; t++) {
-    //     int n, ans, tmp = 0;
-    //     cin >> n;
-    //     for(int i = 0; i < 16; i++) {
-    //         cin >> cost[i];
-    //     }
 
+    for(int i = 0; i < 16; i++) { // 3개씩 짝
+        vector<bool> cur = toMbti(i);
+        vector<bool> sel(4);
+        fill(sel.end()-2, sel.end(), true);
+        do {
+            vector<int> item = {0, i};
+            vector<bool> mbti = cur;
+            for(int j = 0; j < 4; j++) {
+                if(sel[j]) mbti[j] = !mbti[j];
+            }
+            for(int j = 0; j < 4; j++) {
+                if(sel[j]) continue;
+                mbti[j] = !mbti[j];
+                item.push_back(toIdx(mbti));
+                mbti[j] = !mbti[j];
+            }
+            pairs.push_back(item);
+        } while(next_permutation(sel.begin(), sel.end()));
+    }
 
+    int T;
+    cin >> T;
 
-    //     for(int i = 0; i < 72; i++) {
-    //         for(int j = 1; j < (int) pairs[i].size(); j++) {
-    //             tmp += cost[pairs[i][j]];
-    //         }
-    //         pairs[i][0] = tmp;
-    //         tmp = 0;
-    //     }
-    //     sort(pairs.begin(), pairs.end());
-    //     ans = pairs[0][0];
-    //     if(n != 1) {
-    //         vector<vector<int>> prevs;
-    //         prevs.push_back(pairs[0]);
-    //         tmp = 1;
-    //         for (int i = 1; i < n; i++) {
-    //             while(tmp < (int)pairs.size() && isOverlap(prevs, pairs[tmp])) tmp++;
-    //             if(tmp == pairs.size()) break;
-    //             prevs.push_back(pairs[tmp]);
-    //             ans += pairs[tmp][0];
-    //             tmp++;
-    //         }
-    //     }
-    //     cout << "#" << t << " " << ans << endl;
-    // }
+    for(int t = 1; t <= T; t++) {
+        int n, ans, tmp = 0;
+        cin >> n;
+        for(int i = 0; i < 16; i++) {
+            cin >> cost[i];
+        }
+
+        for(int i = 0; i < (int)pairs.size(); i++) {
+            for(int j = 1; j < (int) pairs[i].size(); j++) {
+                tmp += cost[pairs[i][j]];
+            }
+            pairs[i][0] = tmp;
+            tmp = 0;
+        }
+        sort(pairs.begin(), pairs.end());
+
+        ans = pairs[0][0];
+        if(n != 1) {
+            vector<vector<int>> prevs;
+            prevs.push_back(pairs[0]);
+            tmp = 1;
+            for (int i = 1; i < n; i++) {
+                while(tmp < (int)pairs.size() && isOverlap(prevs, pairs[tmp])) tmp++;
+                if(tmp == pairs.size()) break;
+                prevs.push_back(pairs[tmp]);
+                ans += pairs[tmp][0];
+                tmp++;
+            }
+        }
+
+        cout << "#" << t << " " << ans << endl;
+    }
+    return 0;
 }
