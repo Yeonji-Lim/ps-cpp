dc805f140781d129fb4a34d97a3db980e2670ee0
Yeonji-Lim <view7186@naver.com>
2022-06-26 02:48:08 +0900

    시간초과

--- Algorithm/Implement/Example/Jandi.cpp
+++ Algorithm/Implement/Example/Jandi.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-long long cut(vector<int> &v, int k, int d) {
+long long cut1(vector<int> &v, int k, int d) {
     long long ret = 0;
     for(int i = v.size()-1; i > (int)v.size()-k-1; i--) {
         ret += (long long)v[i]-1+d;
@@ -13,27 +14,35 @@ long long cut(vector<int> &v, int k, int d) {
     return ret;
 }
 
+long long cut(priority_queue<int> &pq, int k, int d) {
+    long long ret = 0;
+    for(int i = 0; i < k; i++) {
+        ret += pq.top()-1+d;
+        pq.pop();
+        pq.push(1-d);
+    }
+    return ret;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     int T, n, m, D, k, tmp;
     cin >> T;
     for(int t = 1; t <= T; t++) {
-        vector<int> v;
+        priority_queue<int> pq;
         long long ans = 0;
         cin >> n >> m >> D;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 cin >> tmp;
-                v.push_back(tmp);
+                pq.push(tmp);
             }
         }
         
-        sort(v.begin(), v.end());
-
         for(int d = 1; d <= D; d++) {
             cin >> k;
-            ans += d * cut(v, k, d);
+            ans += d * cut(pq, k, d);
         }
         cout << "#" << t << " " << ans << endl;
     }
