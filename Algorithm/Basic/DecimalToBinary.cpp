8ae98430c930fb79d529ddf52266a140bd9ce0d7
Yeonji-Lim <view7186@naver.com>
2022-07-05 15:29:52 +0900

    10진수에서 2진수로

---
+++ Algorithm/Basic/DecimalToBinary.cpp
@@ -0,0 +1,29 @@
+// 10진수에서 2진수로
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+    int a = 26; // 11010 = 2 + 8 + 16 = 26 
+    int cal = 1;
+    char str[6];
+    int si = 0;
+
+    // 26  = 1 1 0 1 0   
+    for (int i = a; i > 0; i = i >> 1) {
+        if ((i & 1)) {
+            str[si] = '1';
+        }
+        else {
+            str[si] = '0';
+        }
+        si++;
+    }
+    for (int i = a; i > 0; i = i >> 1) {
+        printf("%c", str[--si]);
+    }
+    return 0;
+}
