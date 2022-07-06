0d6a3e4c6b04c585c8e24991ddcf92c126957cc2
Yeonji-Lim <view7186@naver.com>
2022-07-06 12:11:15 +0900

    Failed

--- BOJ/1927/1927.cpp
+++ BOJ/1927/1927.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-deque<int> min_heap;
+int min_heap[1024*128*2];
+int ti = 1;
 
 void swap(int i, int j) {
     int tmp = min_heap[i];
@@ -9,52 +10,52 @@ void swap(int i, int j) {
 }
 
 void setHeapInsert() {
-    if(min_heap.size() == 2) return;
-    int i = min_heap.size()-1;
+    if(ti == 2) return;
+    int i = ti-1, j = (i >> 1);
     while(i > -1) {
-        if(min_heap[i/2] < min_heap[i]) break;
-        swap(i/2, i);
-        i /= 2;
+        if(min_heap[j] < min_heap[i]) break;
+        swap(j, i);
+        i = j;
+        j = (i >> i);
     }
 }
 
 void setHeapDelete() {
-    if(min_heap.size() == 2) return;
-    int i = 1, j;
-    while(i < min_heap.size()) {
-        if(2*i >= min_heap.size()) break;
-        if(min_heap[i] < min_heap[2*i]) {
-            if(2*i+1 >= min_heap.size()) break;
-            if(min_heap[i] < min_heap[2*i+1]) break;
+    if(ti == 2) return;
+    int i = 1, l, r;
+    while(i < ti) {
+        l = i*2; r = i*2+1;
+        if(l >= ti) break;
+        if(min_heap[i] < min_heap[l]) {
+            if(r >= ti) break;
+            if(min_heap[i] < min_heap[r]) break;
         }
-        if(min_heap[2*i] < min_heap[2*i+1]) j = 2*i;
-        else j = 2*i+1;
-        swap(i, j);
-        i = j;
+        if(min_heap[l] > min_heap[r]) l++;
+        swap(i, l);
+        i = l;
     }
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int n, num;
     cin >> n;
-    min_heap.push_back(-1);
+    min_heap[0] = -1;
     for(int i = 0; i < n; i++) {
         cin >> num;
         if(num == 0) {
-            if(min_heap.size() == 1) {
+            if(ti == 1) {
                 cout << 0 << endl;
                 continue;
             }
             cout << min_heap[1] << endl;
-            min_heap.erase(min_heap.begin()+1);
-            if(min_heap.size() == 1) continue;
-            num = min_heap.back();
-            min_heap.pop_back();
-            min_heap.push_front(num);
-            swap(0, 1);
+            min_heap[1] = min_heap[--ti];
+            if(ti == 1) continue;
             setHeapDelete();
         } else {
-            min_heap.push_back(num);
+            min_heap[ti] = num;
+            ti++;
             setHeapInsert();
         }
     }
