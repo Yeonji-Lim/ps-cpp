c3b92183f72289151d206dc7f4150e84f735d63f
Yeonji-Lim <view7186@naver.com>
2022-07-11 14:27:40 +0900

    시간초과

---
+++ BOJ/1717/1717.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> parent(1000000, 0);
+
+int group(int a) {
+    if(parent[a] == a) return a;
+    return parent[a] = group(parent[a]);
+}
+
+void join(int a, int b) {
+    a = group(a);
+    b = group(b);
+    a < b? parent[b] = a: parent[a] = b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n, m, a, b, c;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) parent[i] = i;
+    for(int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        if(a == 0) {
+            join(b, c);
+        } else {
+            if(group(b) == group(c)) cout << "YES\n";
+            else cout << "NO\n";
+        }
+    }
+    return 0;
+}
