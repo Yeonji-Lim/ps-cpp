52cbb199925e2c7ed58245434e54de36616e8d05
Yeonji-Lim <view7186@naver.com>
2022-07-12 20:22:22 +0900

    Failed 리팩토링 했는데 실패

--- BOJ/11438/11438_sol.cpp
+++ BOJ/11438/11438_sol.cpp
@@ -1,10 +1,10 @@
+// 백준에서는 테스트 케이스 각각에 1.5초, 삼성은 각각의 테스트 케이스가 아니라 뭉탱이로 테스트 케이스 제공 -> 초기화에 신경 써야 한다.
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, a, b;
 vector<int> tree[100001];
 int depth[100001];
 deque<int> targets;
-
 int parent[18][100001]; // 2^17 까지 커버 가능하게 [k][v] : v의 2^k번째 부모 저장
 
 int main() {
@@ -18,11 +18,14 @@ int main() {
         tree[b].push_back(a);
     }
 
+    // 전역변수로 정의된 배열을 모두 0이나 -1로 초기화 하는 방법
+    memset(parent, 0, sizeof parent);
+
     // 루트 노드로 부터의 depth를 알아야 : DFS or BFS로 알아내면 됨
     // BFS -> 방문 체크 해야 함 : depth를 이용
     // 근데 이때 루트 노드의 depth는 0 -> 1. depth를 -1로 초기화 2. 루트인 경우만 조건을 따로 빼기
     fill_n(depth, 100001, -1);
-    
+
     targets.push_back(1);
     while(!targets.empty()) {
         int curr = targets.front();
@@ -51,39 +54,34 @@ int main() {
     for(int i = 0; i < m; i++) {
         cin >> a >> b;
         // a, b 의 LCA를 구하여 출력
-        cnt = 0;
-        if(depth[a] > depth[b]) {
-            diff = depth[a] - depth[b];
-            while(diff) {
-                if(diff&1) {
-                    a = parent[cnt][a];
-                }
-                cnt++;
-                diff = diff>>1;
-            }
-        } else if(depth[a] < depth[b]) {
-            diff = depth[b] - depth[a];
-            while(diff) {
-                if(diff&1) {
-                    b = parent[cnt][b];
-                }
-                cnt++;
-                diff = diff>>1;
-            }
+
+        // 1 : Depth 맞추기
+        if(depth[a] < depth[b]) { // a, b에 대한 질문과 b, a의 질문은 같다. 
+            cnt = a;
+            a = b;
+            b = cnt;
         }
+        diff = depth[a] - depth[b];
+        for(int r = 0; diff; r++) {
+            if(diff & 1) {
+                a = parent[r][a];
+            }
+            diff >>= 1;
+        }
+
+        // 2 : LCA 찾기
         while(a != b) {
             cnt = 0;
-            pa = parent[cnt][a]; pb = parent[cnt][b];
-            while(pa != pb) {
-                pa = parent[cnt][a];
-                pb = parent[cnt][b];
-                cnt++;
+            for(cnt = 0; cnt < 18; cnt++) {
+                if(parent[cnt][a] == parent[cnt][b]) break;
             }
-            if(cnt == 0) {
-                a = pa; break;
-            }
-            a = parent[cnt-1][a];
-            b = parent[cnt-1][b];
+            if(cnt) --cnt; 
+            // cnt = 1 : cnt가 0이 되서 다시 while
+            // cnt = 0 : while 조건에 걸러짐
+            // cnt = 18 : 구조상 17만큼 올라가면 모두 만나게 되어 있음
+            //            루트를 넘어가면 0이 들어감, 0이 들어가면 그 전의 값으로 가기 때문에 오동작하지 않음
+            a = parent[cnt][a];
+            b = parent[cnt][b];
         }
         cout << a << '\n';
     }
