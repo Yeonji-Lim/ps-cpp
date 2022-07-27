7faa7f4ea81ce79335e9679dad991a269a0469ee
Yeonji-Lim <view7186@naver.com>
2022-07-27 14:56:11 +0900

    Solved - 2868KB 12ms

--- BOJ/1039/1039.cpp
+++ BOJ/1039/1039.cpp
@@ -11,10 +11,11 @@ string swap(string str, int i, int j) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    int k;
+    int k, len;
     string str;
     cin >> str >> k;
-    if(str.length() == 1) {
+    len = str.length();
+    if(len == 1) {
         cout << "-1\n";
         return 0;
     }
@@ -22,20 +23,21 @@ int main() {
     int t = 0, nt = 1, cnt = 0;
     pq[t].push(str);
     string prev = "";
-    while(!pq[t].empty()) {
-        if(cnt == k) break;
-        str = pq[t].top();
-        pq[t].pop();
-        if(prev == str) continue;
-        prev = str;
-        for(int i = 0; i < str.length()-1; i++) {
-            for(int j = i+1; j < str.length(); j++) {
-                if(i == 0 && str[j] == '0') continue;
-                pq[nt].push(swap(str, i, j));
+    while(cnt < k) {
+        while(!pq[t].empty()) {
+            str = pq[t].top();
+            pq[t].pop();
+            if(prev == str) continue;
+            prev = str;
+            for(int i = 0; i < len; i++) {
+                for(int j = i+1; j < len; j++) {
+                    if(i == 0 && str[j] == '0') continue;
+                    pq[nt].push(swap(str, i, j));
+                }
             }
         }
         t = nt;
-        nt = (nt+1)&1;
+        nt = (nt + 1) & 1;
         cnt++;
         prev = "";
     }
