9fa135977abecb71370ae7e692e4127f5c5ba327
Yeonji-Lim <view7186@naver.com>
2022-07-27 22:48:04 +0900

    Solved - 2020KB 3656ms

--- BOJ/9663/9663.cpp
+++ BOJ/9663/9663.cpp
@@ -1,41 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> done(15, vector<int>(15, -1));
 int n, ans = 0;
+int qn_info[17];
 
-void queen(vector<vector<bool>> x, int ci, int cj, int cnt) {
-    if(cnt == n) { 
-        ans++; return;
-    }
-    done[ci][cj] = 0;
-    x[ci][cj] = true;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(i == ci || j == cj || i+j == ci+cj || j-i == cj-ci) {
-                x[i][j] = true;
+void put_qn(int row) {
+    for(int col = 1; col <= n; col++) {
+        // col이 적합한지 체크
+        // 해당 세로줄에 이미 퀸이 있는 경우
+        if(qn_info[col] != 0) continue;
+
+        // 대각선이 겹치는 경우
+        bool valid = true;
+        for(int j = 1; j <= n; j++) {
+            if(qn_info[j] != 0 && (qn_info[j]+j == row+col || qn_info[j]-j == row-col)) {
+                valid = false;
+                break;
             }
         }
-    }
+        if(!valid) continue;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(done[i][j] == -1 && !x[i][j]) {
-                queen(x, i, j, cnt+1);
-            }
-        }
+        // (row, col)이 적합한 위치인 경우
+        qn_info[col] = row; // 위치 기록
+        if(row == n) ans++; // 마지막 줄에서는 ans++
+        else put_qn(row+1); // 다음 줄로 넘어가기
+        qn_info[col] = 0; // 원상복귀
     }
-    done[ci][cj] = 1;
-    return;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    vector<vector<bool>> x(n, vector<bool>(n, false));
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(done[i][j] == -1) queen(x, i, j, 1);
-        }
-    }
-    cout << ans << endl;
+    put_qn(1);
+    cout << ans << '\n';
     return 0;
 }
