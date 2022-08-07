38d32c8d3a205748e36d0d606eaad38a4e6be97d
Yeonji-Lim <57888020+Yeonji-Lim@users.noreply.github.com>
2022-08-07 20:21:52 +0900

    백트래킹 적용 전

--- BOJ/2580/2580.cpp
+++ BOJ/2580/2580.cpp
@@ -1,17 +1,34 @@
 # include <bits/stdc++.h>
 using namespace std;
-int board [10][10];
-int pos_info [10][10];
-queue<int> numq [10];
+int board[10][10];
+bool rows[10][10];
+bool cols[10][10];
+bool square[10][10];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
-    for(int i = 1; i < 10; i++) {
-        for(int j = 1; j < 10; j++) {
-            cin >> board[i][j];
-            pos_info[board[i][j]][j] = i;
+    for(int row = 1; row < 10; row++) {
+        for(int col = 1; col < 10; col++) {
+            cin >> board[row][col];
+            rows[row][board[row][col]] = true;
+            cols[col][board[row][col]] = true;
+            square[(row-1)/3*3+(col-1)/3+1][board[row][col]] = true;
         }
     }
+    for(int row = 1; row < 10; row++) {
+        for(int col = 1; col < 10; col++) {
+            if(board[row][col] == 0) {
+                for(int tar = 1; tar < 10; tar++){
+                    if(!rows[row][tar] && !cols[col][tar] && !square[(row-1)/3*3+(col-1)/3+1][board[row][col]]) {
+                        rows[row][tar] = true;
+                        cols[col][tar] = true;
+                        square[(row-1)/3*3+(col-1)/3+1][board[row][col]] = true;
+                        board[row][col] = tar;
+                    }
+                }
+            }
+        }
+    } 
     
 }
