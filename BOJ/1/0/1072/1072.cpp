46bd2e891ba00cbabf32009bfaeebcb5271aeba1
yeonji <view7186@naver.com>
2022-08-17 16:25:03 +0900

    Failed

--- BOJ/1/0/1072/1072.cpp
+++ BOJ/1/0/1072/1072.cpp
@@ -7,15 +7,13 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
     cin >> x >> y;
-    z = ((double) y * 100 / x);
+    z = y * 100 / x;
     for(int i = 1; i < 100-z; i++) {
         k = ((z+i)*x-100*y) / (100-(z+i));
         if(k > 0) break;
     }
     if(k > 0) {
-        while ((long long) ((double) (y+k) * 100 / (x+k)) == z) {
-            k++;
-        }
+        while ((long long) (y+k) * 100 / (x+k) < z+1) k++;
         cout << k << '\n';
     }
     else cout << "-1\n";
