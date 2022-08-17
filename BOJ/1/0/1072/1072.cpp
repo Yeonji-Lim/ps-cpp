71fe75a042c1dfd6a0ef4ab82316c1eb0357484f
yeonji <view7186@naver.com>
2022-08-17 16:03:14 +0900

    Failed - 70% - 50 29 케이스는 해결함

--- BOJ/1/0/1072/1072.cpp
+++ BOJ/1/0/1072/1072.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-long long x, y;
-int z, k = 0;
+long long x, y, z, k = 0;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
     cin >> x >> y;
-    z = ((double) y / x) * 100;
+    z = ((double) y * 100 / x);
     for(int i = 1; i < 100-z; i++) {
-        k = (100*y-(z+i)*x) / ((z+i)-100);
+        k = ((z+i)*x-100*y) / (100-(z+i));
         if(k > 0) break;
     }
     if(k > 0) {
-        while ((int) (((double) (y+k) / (x+k)) * 100) == z) k++;
+        while ((long long) ((double) (y+k) * 100 / (x+k)) == z) {
+            k++;
+        }
         cout << k << '\n';
     }
     else cout << "-1\n";
