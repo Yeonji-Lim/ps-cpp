fc5486daa9a79e0d4fc55461b8bb765c6871d762
Yeonji-Lim <view7186@naver.com>
2022-08-20 20:01:29 +0900

    sqrt(n)까지만 가도록 하여 효율성 증가

--- Programmers/zb/week3/Solution1.java
+++ Programmers/zb/week3/Solution1.java
@@ -2,22 +2,19 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-
-    boolean[] pn = new boolean[100];
-
     public int solution(int n) {
+        int[] pn = new int[n];
         int answer = 0;
-        Arrays.fill(pn, true);
-        pn[0] = false;
-        pn[1] = false;
-        for(int i = 2; i < n; i++) {
-            if(pn[i]) {
-                answer++;
-                for(int j = i*2; j < n; j += i) {
-                    pn[j] = false;
+        Arrays.fill(pn, 1);
+        pn[0] = 0;
+        pn[1] = 0;
+        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+            if (pn[i] == 1) {
+                for (int j = i * 2; j < n; j += i) {
+                    pn[j] = 0;
                 }
             }
         }
-        return answer;
+        return Arrays.stream(pn).sum();
     }
 }
