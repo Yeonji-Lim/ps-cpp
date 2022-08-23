7d7a65f31537925a5dfa87e4a9474a4c9c7986a4
yeonji <view7186@naver.com>
2022-08-23 12:39:32 +0900

    배열에서 원소를 추가할 때 뒤에서 부터 하면 더 간단함

--- Algorithm/Basic/TimeInArray.cpp
+++ Algorithm/Basic/TimeInArray.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 void insert(int idx, int num, int arr[], int& len){
-    len++;
-    int tmp = arr[idx];
-    for(int i = idx; i < len; i++) {
-        arr[i] = num;
-        num = tmp;
-        tmp = arr[i+1];
+    for(int i = len; i > idx; i--) {
+        arr[i] = arr[i-1];
     }
+    arr[idx] = num;
+    len++;
 }
 
 void erase(int idx, int arr[], int& len){
