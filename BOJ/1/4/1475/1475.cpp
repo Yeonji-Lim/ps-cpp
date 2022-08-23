cc06372a233ab41e209f78b7f697fdad13b5daaa
yeonji <view7186@naver.com>
2022-08-23 15:17:46 +0900

    Failed - 111666 반례 : 6이 제일 많다고 생각하고 2개로 찍어버림, 같거나 큰 경우에 대해서 처리 해주어야 함

---
+++ BOJ/1/4/1475/1475.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, idx, answer = 0, maxidx;
+int num[10];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    while(n != 0) {
+        idx = n%10;
+        num[idx]++;
+        if(answer < num[idx]) {
+            answer = num[idx];
+            maxidx = idx;
+        }
+        n /= 10;
+    }
+    if(maxidx == 6 || maxidx == 9) {
+        answer = num[6] + num[9];
+        if(answer&1) answer++;
+        answer = answer >> 1;
+    }
+    cout << answer;
+}
