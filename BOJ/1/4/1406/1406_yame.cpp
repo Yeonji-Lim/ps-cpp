561e9f02ff8edce8d30065dda70791ab2ad35be3
Yeonji-Lim <view7186@naver.com>
2022-09-01 00:14:04 +0900

    Sovled - 7576KB 40ms

--- BOJ/1/4/1406/1406_yame.cpp
+++ BOJ/1/4/1406/1406_yame.cpp
@@ -7,78 +7,62 @@ char dat[MAXSIZE];
 int pre[MAXSIZE];
 int nxt[MAXSIZE];
 
-void print()
-{
+void print() {
     int printcur = nxt[0];
-    while (printcur != -1)
-    {
+    while (printcur != -1) {
         cout << dat[printcur];
         printcur = nxt[printcur];
     }
     cout << '\n';
 }
 
-void L()
-{
-    if (cur != 0)
-        cur = pre[cur];
+void L() {
+    if (cur != 0) cur = pre[cur];
 }
 
-void D()
-{
-    if (nxt[cur] != -1)
-        cur = nxt[cur];
+void D() {
+    if (nxt[cur] != -1) cur = nxt[cur];
 }
 
-void insert(char c, int addr)
-{
+void insert(char c, int addr) {
     dat[unused] = c;
     pre[unused] = addr;
     nxt[unused] = nxt[addr];
-    if (nxt[addr] != -1)
-        pre[nxt[addr]] = unused;
+    if (nxt[addr] != -1) pre[nxt[addr]] = unused;
     nxt[addr] = unused;
+    cur = nxt[cur];
     unused++;
-    D();
 }
 
-void erase(int addr)
-{
-    L();
-    nxt[pre[addr]] = nxt[addr];
-    if (nxt[addr] != -1)
-        pre[nxt[addr]] = pre[addr];
+void erase(int addr) {
+    if (addr != 0) {
+        nxt[pre[addr]] = nxt[addr];
+        if (nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+        cur = pre[cur];
+    }
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     fill(pre, pre + MAXSIZE, -1);
     fill(nxt, nxt + MAXSIZE, -1);
     cin >> str >> m;
-    for (int i = 0; i < str.length(); i++)
-    {
+    for (int i = 0; i < str.length(); i++) {
         insert(str[i], cur);
     }
     cin.ignore();
-    while (m--)
-    {
+    while (m--) {
         getline(cin, str);
-        switch (str[0])
-        {
-        case 'L':
-            L();
-            break;
-        case 'D':
-            D();
-            break;
-        case 'B':
-            erase(cur);
-            break;
-        case 'P':
-            insert(str[2], cur);
-            break;
+        switch (str[0]) {
+            case 'L':
+                L(); break;
+            case 'D':
+                D(); break;
+            case 'B':
+                erase(cur); break;
+            case 'P':
+                insert(str[2], cur); break;
         }
     }
     print();
