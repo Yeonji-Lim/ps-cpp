a32b6cc930ecd7f23835d3592047ad918aace707
yeonji <view7186@naver.com>
2022-09-14 16:12:52 +0900

    Failed

---
+++ BOJ/5430/5430.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, n;
+string p, arrstr;
+deque<int> dq;
+bool front = true, error = false;
+
+deque<int> stringToDeque() {
+    deque<int> retdq;
+    stringstream sstream(arrstr.substr(1, arrstr.size()-2));
+    string numstr;
+    while(getline(sstream, numstr, ',')) {
+        retdq.push_back(stoi(numstr));
+    }
+    return retdq;
+}
+
+string DequeToString() {
+    string retstr = "[";
+    if(front) {
+        while(!dq.empty()) {
+            retstr += to_string(dq.front());
+            retstr += ",";
+            dq.pop_front();
+        }
+    } else {
+        while(!dq.empty()) {
+            retstr += to_string(dq.back());
+            retstr += ",";
+            dq.pop_back();
+        }
+    }
+    retstr[retstr.size()-1] = ']';
+    return retstr;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> t;
+    while(t--) {
+        cin >> p;
+        cin >> n;
+        cin >> arrstr;
+        dq = stringToDeque();
+        for(int i = 0; i < p.size(); i++) {
+            if(p[i] == 'R') {
+                front = !front;
+            } else {
+                if(dq.empty()) {
+                    cout << "error\n";
+                    error = true;
+                    break;
+                }
+                if(front) dq.pop_front(); 
+                else dq.pop_back();
+            }
+        }
+        if(!error) cout << DequeToString() << '\n';
+        front = true;
+        error = false;
+    }
+}
