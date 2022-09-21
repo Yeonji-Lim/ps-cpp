45098790e49f9942e1b3020798e593268ad75ab4
yeonji <view7186@naver.com>
2022-09-21 11:11:32 +0900

    Failed - Compile Error - reference to ‘visit’ is ambiguous : std 내에 visit가 존재하므로 사용해서는 안된다. - https://en.cppreference.com/w/cpp/utility/variant/visit

---
+++ BOJ/1/9/1926/1926.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define I first
+#define J second
+using namespace std;
+int n, m, num = 0, ms = 0, cs;
+int board[501][501];
+bool visit[501][501];
+queue<pair<int, int>> q;
+
+int di[4] = {-1, 0, 1, 0};
+int dj[4] = {0, 1, 0, -1};
+bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < m; }
+
+pair<int, int> cur;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> board[i][j];
+            visit[i][j] = false;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(board[i][j] && !visit[i][j]) {
+                num++; cs = 0;
+                visit[i][j] = true;
+                cur = {i, j};
+                q.push(cur);
+                while(!q.empty()) {
+                    cur = q.front(); q.pop();
+                    cs++;
+                    for(int k = 0; k < 4; k++) {
+                        int ni = cur.I + di[k], nj = cur.J + dj[k];
+                        if(isInMap(ni, nj) && board[ni][nj] && !visit[ni][nj]) {
+                            visit[ni][nj] = true;
+                            q.push({ni, nj});
+                        }
+                    }
+                }
+                ms = cs > ms? cs: ms;
+            }
+        }
+    }
+    
+    cout << num << '\n' << ms << '\n';
+}
