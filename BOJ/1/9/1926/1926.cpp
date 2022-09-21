ff7460415ebde094d17d64e27cd05053cb7e8d66
yeonji <view7186@naver.com>
2022-09-21 11:18:46 +0900

    Refactoring

--- BOJ/1/9/1926/1926.cpp
+++ BOJ/1/9/1926/1926.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int n, m, num = 0, ms = 0, cs;
 int board[501][501];
-bool visited[501][501];
+bool vis[501][501];
 queue<pair<int, int>> q;
 
 int di[4] = {-1, 0, 1, 0};
@@ -21,30 +21,28 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> board[i][j];
-            visited[i][j] = false;
+            vis[i][j] = false;
         }
     }
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(board[i][j] && !visited[i][j]) {
-                num++; cs = 0;
-                visited[i][j] = true;
-                cur = {i, j};
-                q.push(cur);
-                while(!q.empty()) {
-                    cur = q.front(); q.pop();
-                    cs++;
-                    for(int k = 0; k < 4; k++) {
-                        int ni = cur.I + di[k], nj = cur.J + dj[k];
-                        if(isInMap(ni, nj) && board[ni][nj] && !visited[ni][nj]) {
-                            visited[ni][nj] = true;
-                            q.push({ni, nj});
-                        }
+            if(board[i][j] == 0 || vis[i][j]) continue;
+            num++; cs = 0;
+            vis[i][j] = true;
+            q.push({i, j});
+            while(!q.empty()) {
+                cur = q.front(); q.pop();
+                cs++;
+                for(int k = 0; k < 4; k++) {
+                    int ni = cur.I + di[k], nj = cur.J + dj[k];
+                    if(isInMap(ni, nj) && board[ni][nj] && !visited[ni][nj]) {
+                        visited[ni][nj] = true;
+                        q.push({ni, nj});
                     }
                 }
-                ms = cs > ms? cs: ms;
             }
+            ms = max(ms, cs);
         }
     }
     
