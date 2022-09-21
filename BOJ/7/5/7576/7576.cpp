3376a6abad47b9779eeed75026093c74189f2cb4
yeonji <view7186@naver.com>
2022-09-21 18:49:30 +0900

    막혀진 곳을 인식하지 못함

---
+++ BOJ/7/5/7576/7576.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define I first
+#define J second
+using namespace std;
+int n, m, ans = 0;
+int box[1001][1001];
+queue<pair<int, int>> q1;
+queue<pair<int, int>> q2;
+bool qp = true;
+
+int di[4] = {-1, 0, 1, 0};
+int dj[4] = {0, 1, 0, -1};
+bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < m; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> box[i][j];
+            if(box[i][j] == 1) q1.push({i, j});
+        }
+    }
+    pair<int, int> cur;
+    while(!q1.empty() || !q2.empty()) {
+        if(qp) {
+            while(!q1.empty()) {
+                cur = q1.front(); q1.pop();
+                for(int k = 0; k < 4; k++) {
+                    int ni = cur.I + di[k], nj = cur.J + dj[k];
+                    if(isInMap(ni, nj) && box[ni][nj] == 0) {
+                        box[ni][nj] = 1;
+                        q2.push({ni, nj});
+                    }
+                }
+            }
+        } else {
+            while(!q2.empty()) {
+                cur = q2.front(); q2.pop();
+                for(int k = 0; k < 4; k++) {
+                    int ni = cur.I + di[k], nj = cur.J + dj[k];
+                    if(isInMap(ni, nj) && box[ni][nj] == 0) {
+                        box[ni][nj] = 1;
+                        q1.push({ni, nj});
+                    }
+                }
+            }
+        }
+        qp = !qp;
+        ans++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(box[i][j] == 0) {
+                cout << -1;
+                return 0;
+            }
+        }
+    }
+    cout << ans-1 << '\n';
+}
