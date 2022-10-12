1f22364604ee70fb4b4663927f177c3e348ffe55
yeonji <view7186@naver.com>
2022-10-12 18:50:36 +0900

    Refactoring - 미리 전체에 대해서 구하고 n이 주어질 때는 계산해두었던 값을 주는 것이 낫다

--- BOJ/9/0/9095/9095.cpp
+++ BOJ/9/0/9095/9095.cpp
@@ -6,17 +6,16 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    fill_n(dp, 11, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for(int i = 4; i < 11; i++) {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
     int n, T;
     cin >> T;
     while(T--) {
         cin >> n;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for(int i = 4; i <= n; i++) {
-            if(dp[i] == 0) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        }
         cout << dp[n] << '\n';
     }
 }
