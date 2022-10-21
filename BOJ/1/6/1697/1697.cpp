64bc8a0ec66cb1067ba451d27db3c0f7598c5609
yeonji <view7186@naver.com>
2022-10-21 18:49:45 +0900

    Failed

--- BOJ/1/6/1697/1697.cpp
+++ BOJ/1/6/1697/1697.cpp
@@ -1,21 +1,39 @@
 #include <bits/stdc++.h>
+#define MAXS 20
 using namespace std;
-int n, k;
-int dist[100001];
-
-int findSec(int cur) {
-    if(cur < 0) return 100002;
-    if(dist[cur] == -1) {
-        if(cur < k) dist[cur] = min(findSec(cur+1), min(findSec(cur-1), findSec(cur*2))) + 1;
-        else dist[cur] = cur-k;  
-    }
-    return dist[cur];
-}
+int n, k, cur, d;
+int dist[MAXS];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> k;
-    fill_n(dist, 100001, -1);
-    cout << findSec(n) << '\n';
+    fill_n(dist, MAXS, -1);
+    queue<int> q;
+    q.push(n);
+    dist[n] = 0;
+    while(!q.empty()) {
+        cur = q.front(); q.pop();
+        d = dist[cur];
+        if(cur-1 >= 0) {
+            if(dist[cur-1] == -1) {
+                dist[cur-1] = d+1;
+                if(cur-1 != k) q.push(cur-1);
+            }
+        }
+        if(cur+1 < MAXS && cur+1 <= k) {
+            if(dist[cur+1] == -1) {
+                dist[cur+1] = d+1;
+                if(cur+1 != k) q.push(cur+1);
+            }
+        }
+        if(cur*2 < MAXS) {
+            if(dist[cur*2] == -1) {
+                dist[cur*2] = d+1;
+                if(cur*2 < k) q.push(cur*2);
+                else dist[k] = min(dist[k], dist[cur*2]+cur-k);
+            }
+        }
+    }
+    cout << dist[k] << '\n';
 }
