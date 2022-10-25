b6cdd425f2a4bdff6dc52f14127c018723df09a9
yeonji <view7186@naver.com>
2022-10-25 18:57:50 +0900

    Solving

--- BOJ/10/0/10026/10026.cpp
+++ BOJ/10/0/10026/10026.cpp
@@ -13,7 +13,6 @@ bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < n; }
 
 void bfs(int i, int j) {
     queue<pair<int, int>> q;
-    char color = board[i][j];
     vis[i][j] = true;
     q.push({i, j});
     pair<int, int> cur;
@@ -22,11 +21,27 @@ void bfs(int i, int j) {
         cur = q.front(); q.pop();
         for(int k = 0; k < 4; k++) {
             ni = cur.I + di[k]; nj = cur.J + dj[k];
-            if(isInMap(ni, nj) && !vis[ni][nj]) {
-                if(color == board[ni][nj]) {
-                    vis[ni][nj];
-                    q.push({ni, nj});
-                }
+            if(isInMap(ni, nj) && !vis[ni][nj] && board[i][j] == board[ni][nj]) {
+                vis[ni][nj] = true;
+                q.push({ni, nj});
+            }
+        }
+    }
+}
+
+void weakbfs(int i, int j) {
+    queue<pair<int, int>> q;
+    vis[i][j] = true;
+    q.push({i, j});
+    pair<int, int> cur;
+    int ni, nj;
+    while(!q.empty()) {
+        cur = q.front(); q.pop();
+        for(int k = 0; k < 4; k++) {
+            ni = cur.I + di[k]; nj = cur.J + dj[k];
+            if(isInMap(ni, nj) && !vis[ni][nj] && board[ni][nj] != 'B') {
+                vis[ni][nj] = true;
+                q.push({ni, nj});
             }
         }
     }
@@ -46,8 +61,29 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(!vis[i][j]) {
+                ans++;
+                if(board[i][j] == 'B') weakans++;
                 bfs(i, j);
             }
         }
     }
+
+    if(weakans != 0) {
+        fill(&vis[0][0], &vis[n][n], false);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!vis[i][j] && board[i][j] != 'B') {
+                    weakans++;
+                    weakbfs(i, j);
+                }
+            }
+        }
+    } else weakans = 1;
+    cout << ans << ' ' << weakans << '\n';
 }
+
+/**
+ * @brief 
+ * B인 경우의 개수를 미리 저장해두기 weakans에
+ * 그러고 나서 G, R에 대해서 다시 bfs 진행
+ */
