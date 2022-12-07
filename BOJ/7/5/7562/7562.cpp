c3b9a543b4a2d03594b39adbef5618cf6d087265
yeonji <view7186@naver.com>
2022-12-07 11:56:15 +0900

    Solved - 2508KB 24ms

--- BOJ/7/5/7562/7562.cpp
+++ BOJ/7/5/7562/7562.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 int t, n, si, sj, ei, ej, ni, nj, dist, ans = 0;
 int board[301][301];
-queue<pair<int, int>> q;
-pair<int, int> cur;
 
 int d[8][2] = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
 bool isInBoard(int i, int j) {return i > -1 && i < n && j > -1 && j < n; }
@@ -17,13 +15,15 @@ int main() {
     while(t--) {
         cin >> n >> si >> sj >> ei >> ej;
         fill(&board[0][0], &board[n][n], 0);
+        queue<pair<int, int>> q;
+        pair<int, int> cur;
         q.push({si, sj});
         while(!q.empty()) {
             cur = q.front(); q.pop();
             dist = board[cur.I][cur.J];
             if(cur.I == ei && cur.J == ej) {
-                    ans = dist;
-                    break;
+                ans = dist;
+                break;
             }
             for(int k = 0; k < 8; k++) {
                 ni = cur.I + d[k][0]; nj = cur.J + d[k][1];
