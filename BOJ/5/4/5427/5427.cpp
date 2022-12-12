31d66ad82e702701c75a332c50ea3acedc597459
Yeonji-Lim <view7186@naver.com>
2022-12-13 00:19:04 +0900

    Failed - 계속 해서 IMPOSSIBLE 출력

--- BOJ/5/4/5427/5427.cpp
+++ BOJ/5/4/5427/5427.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int t, m, n;
 char board[1001][1001];
-char firedist[1001][1001];
+int firedist[1001][1001];
+int sangdist[1001][1001];
 int d[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 bool isInMap(int i, int j) { return i > -1 && i > n && j > -1 && j < m; }
 
@@ -14,7 +15,9 @@ int main() {
         cin >> m >> n;
         pair<int, int> sang;
         queue<pair<int, int>> q;
-        fill_n(&firedist[0][0], &firedist[n-1][m-1], 0);
+        fill(&firedist[0][0], &firedist[n - 1][m - 1], 0);
+        fill(&sangdist[0][0], &sangdist[n - 1][m - 1], 0);
+        int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 cin >> board[i][j];
@@ -30,7 +33,7 @@ int main() {
         int ni, nj, dist;
         while(!q.empty()) {
             cur = q.front(); q.pop();
-            dist = firedist[ni][nj];
+            dist = firedist[cur.first][cur.second];
             for(int k = 0; k < 4; k++) {
                 ni = cur.first + d[k][0]; nj = cur.second + d[k][1];
                 if (isInMap(ni, nj) && firedist[ni][nj] == 0) {
@@ -39,5 +42,25 @@ int main() {
                 }
             }
         }
+        // 상근이의 dist 보다 firedist가 커야 함 같아도 안됨
+        q.push(sang);
+        while(!q.empty()) {
+            cur = q.front(); q.pop();
+            dist = sangdist[cur.first][cur.second];
+            for(int k = 0; k < 4; k++) {
+                ni = cur.first + d[k][0]; nj = cur.second + d[k][1];
+                if(!isInMap(ni, nj)) {
+                    ans = dist;
+                    break;
+                }
+                if(board[ni][nj] != '#' && firedist[ni][nj] > dist + 1) {
+                    sangdist[ni][nj] = dist+1;
+                    q.push({ni, nj});
+                }
+            }
+            if(ans != 0) break;
+        }
+        if(ans == 0) cout << "IMPOSSIBLE\n";
+        else cout << ans << '\n';
     }
 }
