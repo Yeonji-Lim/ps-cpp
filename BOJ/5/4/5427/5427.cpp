fde800117e3081e368f2520e9bf3ec4427d72ceb
Yeonji-Lim <view7186@naver.com>
2022-12-13 00:31:25 +0900

    Failed - 2번째 예시 입력에 대해서 오답 - 불이 없는 경우에 대해 예외처리 : firedist 초기값 -1 - isInMap 부등호 오타 수정

--- BOJ/5/4/5427/5427.cpp
+++ BOJ/5/4/5427/5427.cpp
@@ -5,7 +5,7 @@ char board[1001][1001];
 int firedist[1001][1001];
 int sangdist[1001][1001];
 int d[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-bool isInMap(int i, int j) { return i > -1 && i > n && j > -1 && j < m; }
+bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < m; }
 
 int main() {
     ios::sync_with_stdio(0);
@@ -15,14 +15,15 @@ int main() {
         cin >> m >> n;
         pair<int, int> sang;
         queue<pair<int, int>> q;
-        fill(&firedist[0][0], &firedist[n - 1][m - 1], 0);
+        fill(&firedist[0][0], &firedist[n - 1][m - 1], -1);
         fill(&sangdist[0][0], &sangdist[n - 1][m - 1], 0);
-        int ans = 0;
+        int ans = -1;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 cin >> board[i][j];
                 if (board[i][j] == '*') {
                     q.push({i, j});
+                    firedist[i][j] = 0;
                 }
                 if (board[i][j] == '@') { // 이렇게 하는 방법 보다 좋은 방법이 있엇던 것 같다.
                     sang = {i, j};
@@ -36,7 +37,7 @@ int main() {
             dist = firedist[cur.first][cur.second];
             for(int k = 0; k < 4; k++) {
                 ni = cur.first + d[k][0]; nj = cur.second + d[k][1];
-                if (isInMap(ni, nj) && firedist[ni][nj] == 0) {
+                if (isInMap(ni, nj) && firedist[ni][nj] == -1) {
                     firedist[ni][nj] = dist+1;
                     q.push({ni, nj});
                 }
@@ -50,17 +51,17 @@ int main() {
             for(int k = 0; k < 4; k++) {
                 ni = cur.first + d[k][0]; nj = cur.second + d[k][1];
                 if(!isInMap(ni, nj)) {
-                    ans = dist;
+                    ans = dist+1;
                     break;
                 }
-                if(board[ni][nj] != '#' && firedist[ni][nj] > dist + 1) {
+                if(board[ni][nj] != '#' && (firedist[ni][nj] > dist + 1 || firedist[ni][nj] == -1)) {
                     sangdist[ni][nj] = dist+1;
                     q.push({ni, nj});
                 }
             }
-            if(ans != 0) break;
+            if(ans != -1) break;
         }
-        if(ans == 0) cout << "IMPOSSIBLE\n";
+        if(ans == -1) cout << "IMPOSSIBLE\n";
         else cout << ans << '\n';
     }
 }
