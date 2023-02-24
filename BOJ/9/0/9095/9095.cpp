b174f3d451e906a9c9bd870f06e11e5013a37170
Yeonji-Lim <view7186@naver.com>
2023-02-24 15:30:32 +0900

    Solved - 2020KB 0ms

--- BOJ/9/0/9095/9095.cpp
+++ BOJ/9/0/9095/9095.cpp
@@ -1,21 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
-int dp[11];
+// #include <bits/stdc++.h>
+// using namespace std;
+// int dp[11];
 
-int main() {
+// int main() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     dp[1] = 1;
+//     dp[2] = 2;
+//     dp[3] = 4;
+//     for(int i = 4; i < 11; i++) {
+//         dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+//     }
+//     int n, T;
+//     cin >> T;
+//     while(T--) {
+//         cin >> n;
+//         cout << dp[n] << '\n';
+//     }
+// }
+
+// 20230224
+#include <iostream>
+using namespace std;
+
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    int dp[11] = {};
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
-    for(int i = 4; i < 11; i++) {
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-    }
-    int n, T;
+    int T;
     cin >> T;
     while(T--) {
+        int n;
         cin >> n;
+        for(int i = 4; i <= n; i++) {
+            if(dp[i] != 0) continue;
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
         cout << dp[n] << '\n';
     }
 }
