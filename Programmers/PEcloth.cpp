586227fd3d3435deb9c377a19ee8ac7a2c3909e0
Yeonji-Lim <view7186@naver.com>
2023-03-15 23:25:18 +0900

    Failed - 84.0/100.0

---
+++ Programmers/PEcloth.cpp
@@ -0,0 +1,31 @@
+// 42862
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    bool donan = false;
+    for(int i = 0; i < reserve.size(); i++) {
+        for(int j = 0; j < lost.size(); j++) {
+            if(reserve[i] == lost[j]) {
+                lost.erase(lost.begin() + j);
+                donan = true;
+                break;
+            }
+        }
+        if(donan) {
+            donan = false;
+            continue;
+        }
+        for(int j = 0; j < lost.size(); j++) {
+            if(lost[j] == reserve[i]+1 || lost[j] == reserve[i]-1) {
+                lost.erase(lost.begin() + j);
+                break;
+            }
+        }
+    }
+    answer = n-lost.size();
+    return answer;
+}
