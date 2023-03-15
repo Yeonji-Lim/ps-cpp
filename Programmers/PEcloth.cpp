5e7aa0a366681f17da56f85ecb531477764165d7
Yeonji-Lim <view7186@naver.com>
2023-03-15 23:42:18 +0900

    Solved 최대한 많은 학생이 체육복을 빌려야 하기 때문에 앞번호 위주로 빌려준 거 뒷번호 위주로 빌려준거 모두 비교해야함

--- Programmers/PEcloth.cpp
+++ Programmers/PEcloth.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int max(int a, int b) { return a > b? a: b; }
+
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
     bool donan = false;
@@ -14,21 +16,30 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
         for(int j = 0; j < lost.size(); j++) {
             if(reserve[i] == lost[j]) {
                 lost.erase(lost.begin() + j);
-                donan = true;
+                reserve.erase(reserve.begin() + i);
+                i--;
                 break;
             }
         }
-        if(donan) {
-            donan = false;
-            continue;
+    }
+    vector<int> temp = lost;
+    for(int i = 0; i < reserve.size(); i++) {
+        for(int j = 0; j < temp.size(); j++) {
+            if(temp[j] == reserve[i]+1 || temp[j] == reserve[i]-1) {
+                temp.erase(temp.begin() + j);
+                break;
+            }
         }
-        for(int j = 0; j < lost.size(); j++) {
+    }
+    answer = n-temp.size();
+    for(int i = 0; i < reserve.size(); i++) {
+        for(int j = lost.size()-1; j > -1; j--) {
             if(lost[j] == reserve[i]+1 || lost[j] == reserve[i]-1) {
                 lost.erase(lost.begin() + j);
                 break;
             }
         }
     }
-    answer = n-lost.size();
+    answer = max(answer, n-lost.size());
     return answer;
 }
