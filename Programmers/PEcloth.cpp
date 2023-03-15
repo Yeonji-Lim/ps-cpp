a2bd0be805afe311db30a505c6cdb66db95e4251
Yeonji-Lim <view7186@naver.com>
2023-03-15 23:33:01 +0900

    Failed - 92.0/100.0 리스트가 정렬이 되어있다는 가정이 없음 그러나 실패

--- Programmers/PEcloth.cpp
+++ Programmers/PEcloth.cpp
@@ -1,12 +1,15 @@
 // 42862
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
     bool donan = false;
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(), reserve.end());
     for(int i = 0; i < reserve.size(); i++) {
         for(int j = 0; j < lost.size(); j++) {
             if(reserve[i] == lost[j]) {
