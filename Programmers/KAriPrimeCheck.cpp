3ecb2b813a33c45b82652651b31278d05a71f023
Yeonji-Lim <view7186@naver.com>
2023-03-20 23:58:21 +0900

    Solved 꼭 소수 모음을 만들 필요는 없다

--- Programmers/KAriPrimeCheck.cpp
+++ Programmers/KAriPrimeCheck.cpp
@@ -1,21 +1,18 @@
 // 92335
 #include <string>
 #include <vector>
-#define MS 10000000001
+#include <cmath>
 
 using namespace std;
 
-vector<bool> prime = vector<bool>(MS);
-long long last_end = 2;
-void set_prime(long long end) {
-    if(last_end > end) return;
-    for(long long i = last_end; i <= end; i++) {
-        if(!prime[i]) continue;
-        for(long long j = i*2; j <= end; j += i) {
-            if(prime[j]) prime[j] = false;
+bool is_prime(long long n) {
+    if(n == 1) return false;
+    for(long long i = 2; i <= sqrt(n); i++) {
+        if(n%i == 0) {
+            return false;
         }
     }
-    last_end = end;
+    return true;
 }
 
 string change_arithmetic(int n, int k) {
@@ -29,8 +26,6 @@ string change_arithmetic(int n, int k) {
 
 int solution(int n, int k) {
     int answer = 0;
-    fill(prime.begin(), prime.end(), true);
-    prime[0] = false; prime[1] = false;
     string k_ari = change_arithmetic(n, k);
     long long num;
     int si = -1;
@@ -38,8 +33,7 @@ int solution(int n, int k) {
         if(k_ari[i] == '0' || i == k_ari.size()) {
             if(si != -1) {
                 num = stoll(k_ari.substr(si, i-si));
-                set_prime(num);
-                if(prime[num]) answer++;
+                if(is_prime(num)) answer++;
                 si = -1;
             }
         } else if(si == -1) si = i;
