b98efe17dd36bced93182773f0622af63ea33e7a
Yeonji-Lim <view7186@naver.com>
2023-03-24 16:17:33 +0900

    Failed - 55.6/100.0 이분탐색으로 바꿨으나 실패

--- Programmers/Immigration.cpp
+++ Programmers/Immigration.cpp
@@ -1,25 +1,31 @@
 // 43238
+
+// 한번에 최소 시간을 구하는 것은 무조건 시간 초과가 날 수 있다.
+// 그런 경우 시점의 전환이 필요하다
+// 원래 : 사람당 어떤 심사관에 대기해야 하는지
+// 바꿈 : 해당 시간 안에 심사관 각각 몇명을 처리할 수 있는 지
+
 #include <string>
 #include <vector>
-#define MT 1000000000000000001
+#include <algorithm>
 
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-    int s = times.size();
-    long long min_time = MT;
-    int min_t;
-    vector<int> wait(s, 1);
-    for(int i = 0; i < n; i++) {
-        for(int t = 0; t < s; t++) {
-            if(min_time > times[t]*wait[t]) {
-                min_time = times[t]*wait[t];
-                min_t = t;
-            }
-        }
-        wait[min_t]++;
-        if(i == n-1) break;
-        min_time = MT;
+    long long answer;
+    long long mint = 1;
+    long long maxt = *max_element(times.begin(), times.end())*n;
+    long long avgt;
+    long long done = 0;
+    while(mint <= maxt) {
+        avgt = (mint+maxt)/2;
+        for(int i = 0; i < times.size(); i++)
+            done += avgt/(long long)(times[i]);
+        if(done >= n) {
+            answer = avgt;
+            maxt = avgt-1;
+        } else mint = avgt+1;
+        done = 0;
     }
-    return min_time;
+    return answer;
 }
