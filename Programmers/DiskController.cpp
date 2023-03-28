f072dc4f8ec3522a860c794e78b72c2d095d4d52
Yeonji-Lim <view7186@naver.com>
2023-03-29 00:09:02 +0900

    Failed 30.0/100.0 현재 아무것도 안하고 있을 때 처리 안함

---
+++ Programmers/DiskController.cpp
@@ -0,0 +1,41 @@
+// 42627
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct cmp {
+    bool operator()(vector<int> a, vector<int> b) { return a[1] > b[1]; }
+};
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    sort(jobs.begin(), jobs.end());
+    priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
+    vector<int> item;
+    int time = 0, i = 0, s = jobs.size();
+    while(1) {
+        if(i == s) {
+            while(!pq.empty()) {
+                item = pq.top();
+                time += item[1];
+                answer += time - item[0];
+                pq.pop();
+            }
+            break;
+        }
+        while(i < s) {
+            if(jobs[i][0] > time) break;
+            pq.push(jobs[i]);
+            i++;      
+        }
+        item = pq.top();
+        time += item[1];
+        answer += time - item[0];
+        pq.pop();
+    }
+    answer /= s;
+    return answer;
+}
