caf89c7c5e602fd982e119402c02ca8352779ec8
Yeonji-Lim <view7186@naver.com>
2023-04-06 17:37:17 +0900

    Solved - BFS로 푸니까 해결

--- Programmers/DFSBFS/GameMap.cpp
+++ Programmers/DFSBFS/GameMap.cpp
@@ -1,33 +1,35 @@
 // 1844
 #include<vector>
-#include<string>
+#include<queue>
 #define MS 101
 using namespace std;
-vector<vector<int>> map;
 int n, m;
-int dist[MS][MS];
 
-int d[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 int isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < m; }
 
-void find(int i, int j) {
-    if(i == n-1 && j == m-1) return;
-    int ni, nj;
-    for(int k = 0; k < 4; k++) {
-        ni = i + d[k][0]; nj = j + d[k][1];
-        if(isInMap(ni, nj) && dist[ni][nj] == -1 && map[ni][nj] == 1) {
-            dist[ni][nj] = dist[i][j]+1;
-            find(ni, nj);
-        }
-    }
-}
-
 int solution(vector<vector<int>> maps) {
-    map = maps;
-    n = maps.size();
-    m = maps[0].size();
+    int d[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int dist[MS][MS];
+    int ci, cj, ni, nj;
+    queue<pair<int, int>> q;
+    
+    n = maps.size(); m = maps[0].size();
     fill(&dist[0][0], &dist[n][m], -1);
     dist[0][0] = 1;
-    find(0, 0);
+    q.push({0, 0});
+    
+    while(!q.empty()) {
+        ci = q.front().first; cj = q.front().second;
+        q.pop();
+        if(ci == n-1 && cj == m-1) break;
+        for(int k = 0; k < 4; k++) {
+            ni = ci + d[k][0]; nj = cj + d[k][1];
+            if(isInMap(ni, nj) && dist[ni][nj] == -1 && maps[ni][nj] == 1) {
+                dist[ni][nj] = dist[ci][cj]+1;
+                q.push({ni, nj});
+            }
+        }
+    }
+    
     return dist[n-1][m-1];
 }
