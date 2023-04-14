d54c217e39a5b45c34393905c53767eed22f7cd2
Yeonji-Lim <view7186@naver.com>
2023-04-14 20:42:50 +0900

    Got Help

--- Goorm/maxmul.cpp
+++ Goorm/maxmul.cpp
@@ -4,15 +4,21 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n, t, cur = 1, ans = 0;
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        t = i;
-        while(t) {
-            cur *= t%10;
-            if(!cur) break;
-            t /= 10;
-        }
+    string number;
+    int t, cur = 1, ans = 0;
+    cin >> number;
+    for(int i = 0; i < number.size(); i++) {
+        cur *= number[i]-'0';
+    }
+    ans = max(cur, ans);
+    cur = 1;
+    for(int i = 0; i < number.size(); i++) {
+        if(i != 0)
+            for(int k = 0; k < i; k++)
+                cur *= number[k]-'0';
+        t = number[i]-'0'-1;
+        if(t > 0 || i != 0) cur *= t;
+        for(int k = 0; k < number.size()-i-1; k++) cur *= 9;
         ans = max(cur, ans);
         cur = 1;
     }
