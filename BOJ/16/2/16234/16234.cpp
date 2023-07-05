bc9f62f3296bdb28d6fa77c4ba04752a73cf00b1
Yeonji-Lim <view7186@naver.com>
2023-07-06 00:12:29 +0900

    Solved - 2172KB 256ms

--- BOJ/16/2/16234/16234.cpp
+++ BOJ/16/2/16234/16234.cpp
@@ -69,76 +69,156 @@
 //     return 0;
 // } 
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n, l, r;
+// vector<vector<int>> globalMap(50, vector<int>(50, 0));
+// vector<vector<bool>> visited(50, vector<bool>(50, false));
+// vector<vector<pair<int, int>>> unions;
+// int d[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+// bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < n; }
+// bool isUnionPossible(int ci, int cj, int ni, int nj) {
+//     int sub = max(globalMap[ci][cj], globalMap[ni][nj]) - min(globalMap[ci][cj], globalMap[ni][nj]);
+//     return l <= sub && sub <= r;
+// }
+
+// vector<pair<int, int>> migration(int i, int j) {
+//     vector<pair<int, int>> unionCountrys;
+//     queue<pair<int, int>> q;
+
+//     visited[i][j] = true;
+//     unionCountrys.emplace_back(i, j);
+//     q.emplace(i, j);
+
+//     while(!q.empty()) {
+//         int ci = q.front().first, cj = q.front().second;
+//         q.pop();
+//         for(int k = 0; k < 4; k++) {
+//             int ni = ci+d[k][0], nj = cj+d[k][1];
+//             if(isInMap(ni,nj) && !visited[ni][nj] && isUnionPossible(ci, cj, ni, nj)) {
+//                 visited[ni][nj] = true;
+//                 unionCountrys.emplace_back(ni, nj);
+//                 q.emplace(ni, nj);
+//             }
+//         }
+//     }
+
+//     return unionCountrys;
+// }
+
+// void seperatePeople(vector<pair<int, int>> unionCountrys) {
+//     int size = (int)unionCountrys.size(), tmp = 0;
+//     for(int i = 0; i < size; i++)
+//         tmp += globalMap[unionCountrys[i].first][unionCountrys[i].second];
+//     tmp /= size;
+//     for(int i = 0; i < size; i++)
+//         globalMap[unionCountrys[i].first][unionCountrys[i].second] = tmp;
+// }
+
+// int main() {
+//     cin >> n >> l >> r;
+//     for(int i = 0; i < n; i++)
+//         for(int j = 0; j < n; j++)
+//             cin >> globalMap[i][j];
+//     int cnt = 1, ans = 0;
+//     do {
+//         cnt = 1;
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(!visited[i][j]) {
+//                     vector<pair<int, int>> unionCountrys = migration(i, j);
+//                     cnt = max(cnt, (int)unionCountrys.size());
+//                     unions.push_back(unionCountrys);
+//                 }
+//             }
+//         }
+//         if(cnt == 1) break;
+//         for(int i = 0; i < (int)unions.size(); i++)
+//             seperatePeople(unions[i]);
+//         fill(visited.begin(), visited.end(), vector<bool>(50, false));
+//         unions.clear();
+//         ans++;
+//     } while(cnt > 1);
+//     cout << ans << endl;
+//     return 0;
+// }
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
-int n, l, r;
-vector<vector<int>> globalMap(50, vector<int>(50, 0));
-vector<vector<bool>> visited(50, vector<bool>(50, false));
-vector<vector<pair<int, int>>> unions;
-int d[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-bool isInMap(int i, int j) { return i > -1 && i < n && j > -1 && j < n; }
-bool isUnionPossible(int ci, int cj, int ni, int nj) {
-    int sub = max(globalMap[ci][cj], globalMap[ni][nj]) - min(globalMap[ci][cj], globalMap[ni][nj]);
-    return l <= sub && sub <= r;
-}
 
-vector<pair<int, int>> migration(int i, int j) {
-    vector<pair<int, int>> unionCountrys;
-    queue<pair<int, int>> q;
+int n, l, r, cnt = 0;
+int board[50][50];
 
-    visited[i][j] = true;
-    unionCountrys.emplace_back(i, j);
-    q.emplace(i, j);
+vector<vector<pair<int, int>>> ucs;
+queue<pair<int, int>> q;
 
-    while(!q.empty()) {
-        int ci = q.front().first, cj = q.front().second;
-        q.pop();
-        for(int k = 0; k < 4; k++) {
-            int ni = ci+d[k][0], nj = cj+d[k][1];
-            if(isInMap(ni,nj) && !visited[ni][nj] && isUnionPossible(ci, cj, ni, nj)) {
-                visited[ni][nj] = true;
-                unionCountrys.emplace_back(ni, nj);
-                q.emplace(ni, nj);
-            }
+int d[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+bool isInBoard(int i, int j) { return i > -1 && i < n && j > -1 && j < n; }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> l >> r;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> board[i][j];
         }
     }
 
-    return unionCountrys;
-}
-
-void seperatePeople(vector<pair<int, int>> unionCountrys) {
-    int size = (int)unionCountrys.size(), tmp = 0;
-    for(int i = 0; i < size; i++)
-        tmp += globalMap[unionCountrys[i].first][unionCountrys[i].second];
-    tmp /= size;
-    for(int i = 0; i < size; i++)
-        globalMap[unionCountrys[i].first][unionCountrys[i].second] = tmp;
-}
-
-int main() {
-    cin >> n >> l >> r;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> globalMap[i][j];
-    int cnt = 1, ans = 0;
     do {
-        cnt = 1;
+        ucs.clear();
+        bool vis[50][50] = {false, };
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(!visited[i][j]) {
-                    vector<pair<int, int>> unionCountrys = migration(i, j);
-                    cnt = max(cnt, (int)unionCountrys.size());
-                    unions.push_back(unionCountrys);
+                if(!vis[i][j]) {
+                    vis[i][j] = true;
+                    q.emplace(i, j);
+                    vector<pair<int, int>> uc;
+                    uc.emplace_back(i, j);
+                    int ci, cj, ni, nj, dist, total = board[i][j];
+
+                    while(!q.empty()) {
+                        ci = q.front().first; cj = q.front().second;
+                        q.pop();
+                        for(int k = 0; k < 4; k++) {
+                            ni = ci + d[k][0]; nj = cj + d[k][1];
+                            if(isInBoard(ni, nj) && !vis[ni][nj]) {
+                                dist = abs(board[ci][cj]-board[ni][nj]);
+                                if(dist >= l && dist <= r) {
+                                    vis[ni][nj] = true;
+                                    q.emplace(ni, nj);
+                                    uc.emplace_back(ni, nj);
+                                    total += board[ni][nj];
+                                }
+                            }
+                        }
+                    }
+                    if(uc.size() == 1) {
+                        vis[i][j] = false;
+                        continue;
+                    }
+                    uc.emplace_back(total, uc.size());
+                    ucs.push_back(uc);
                 }
             }
         }
-        if(cnt == 1) break;
-        for(int i = 0; i < (int)unions.size(); i++)
-            seperatePeople(unions[i]);
-        fill(visited.begin(), visited.end(), vector<bool>(50, false));
-        unions.clear();
-        ans++;
-    } while(cnt > 1);
-    cout << ans << endl;
+        if(ucs.empty()) break;
+        vector<pair<int, int>> uc;
+        int cal;
+        for(int i = 0; i < ucs.size(); i++) {
+            uc = ucs[i];
+            cal = uc[uc.size()-1].first / uc[uc.size()-1].second;
+            for(int j = 0; j < uc.size()-1; j++) {
+                board[uc[j].first][uc[j].second] = cal;
+            }
+        }
+        cnt++;
+    } while(!ucs.empty());
+
+    cout << cnt << '\n';
     return 0;
 }
