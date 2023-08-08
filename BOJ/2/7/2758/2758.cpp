4e4f6a6e1f3ae6036f8355bb91950b765846b652
Yeonji-Lim <view7186@naver.com>
2023-08-08 22:59:34 +0900

    Failed - timeout

--- BOJ/2/7/2758/2758.cpp
+++ BOJ/2/7/2758/2758.cpp
@@ -2,28 +2,30 @@
 
 using namespace std;
 
-int t,n,m,ans;
-int dp[11][2001];
+int t,n,m;
+long long ans = 0, tmp;
+long long dp[11][2001];
 
-int choose(int tn, int tm) {
-    if(tn > tm) return -1;
-	if(dp[tn][tm]!=0) return dp[tn][tm];
-	if(tn == 1) dp[tn][tm] = tm;
-	else {
-	    for(int i=0; 2*i+1 < tm && tn <= tm-2*i; i++) {
-	    	dp[tn][tm] += choose(tn-1,tm-2*i-1);
-	    }
-	}
-	return dp[tn][tm];
+long long choose(int tn, int tm) {
+    if(tm <= 0) return 0;
+    if(dp[tn][tm] != 0) return dp[tn][tm];
+    if(tn == 1) dp[tn][tm] = 1;
+    else if(tm == 1) dp[tn][tm] = 0;
+    else dp[tn][tm] = choose(tn-1, tm/2) + choose(tn, tm-2);
+    return dp[tn][tm];
 }
 
 int main() {
 	cin >> t;
 	while(t--) {
 		cin >> n >> m;
-		ans = choose(n,m);
-        if(ans > 0) cout << ans << '\n';
-        else cout << "-1\n";
+        for(int i = m; i > -1; i--) {
+            tmp = choose(n, i);
+            if(tmp == 0) break;
+            ans += tmp;
+        }
+        cout << ans << '\n';
+        ans = 0;
 	}
 	return 0;
 }
