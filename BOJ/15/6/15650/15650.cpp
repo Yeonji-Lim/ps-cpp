8c3b060c7e4be6fd7ff398b455afdb972fbc7a1e
Yeonji-Lim <view7186@naver.com>
2023-10-07 21:12:31 +0900

    Solved - 2020KB 0ms

--- BOJ/15/6/15650/15650.cpp
+++ BOJ/15/6/15650/15650.cpp
@@ -1,31 +1,62 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int n, m;
+// bool visited[9];
+// vector<int> v;
+
+// void bt(int si) {
+//     visited[si] = true;
+//     v.push_back(si);
+//     if(v.size() == m) {
+//         for(int i = 0; i < m; i++) {
+//             cout << v[i] << " ";
+//         }
+//         cout << '\n';
+//     }else {
+//         for(int i = si+1; i <= n; i++) {
+//             if(!visited[i]) bt(i);
+//         }
+//     }
+//     visited[si] = false;
+//     v.pop_back();
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cout.tie(NULL); cin.tie(NULL);
+//     cin >> n >> m;
+//     for(int i = 1; i <= n; i++) {
+//         bt(i);
+//     }
+// }
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
+bool vst[9];
 int n, m;
-bool visited[9];
 vector<int> v;
 
-void bt(int si) {
-    visited[si] = true;
-    v.push_back(si);
-    if(v.size() == m) {
-        for(int i = 0; i < m; i++) {
-            cout << v[i] << " ";
-        }
+void go(int li, int cnt) {
+    if(cnt == m) {
+        for(int i : v) cout << i << ' ';
         cout << '\n';
-    }else {
-        for(int i = si+1; i <= n; i++) {
-            if(!visited[i]) bt(i);
+        return;
+    }
+    for(int i = li+1; i <= n; i++) {
+        if(!vst[i]) {
+            vst[i] = true;
+            v.push_back(i);
+            go(i, cnt+1);
+            v.pop_back();
+            vst[i] = false;
         }
     }
-    visited[si] = false;
-    v.pop_back();
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cout.tie(NULL); cin.tie(NULL);
     cin >> n >> m;
-    for(int i = 1; i <= n; i++) {
-        bt(i);
-    }
+    go(0, 0);
+    return 0;
 }
