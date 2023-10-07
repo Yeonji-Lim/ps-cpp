07544e59aed45d8e0375ec78e2133897c2f78cc2
Yeonji-Lim <view7186@naver.com>
2023-10-07 21:18:41 +0900

    Solved - 2020KB 492ms

--- BOJ/15/6/15651/15651.cpp
+++ BOJ/15/6/15651/15651.cpp
@@ -1,29 +1,55 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int n, m;
+// vector<int> v;
+
+// void bt(int si) {
+//     v.push_back(si);
+//     if(v.size() == m) {
+//         for(int i = 0; i < m; i++) {
+//             cout << v[i] << " ";
+//         }
+//         cout << '\n';
+//     }else {
+//         for(int i = 1; i <= n; i++) {
+//             bt(i);
+//         }
+//     }
+//     v.pop_back();
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cout.tie(NULL); cin.tie(NULL);
+//     cin >> n >> m;
+//     for(int i = 1; i <= n; i++) {
+//         bt(i);
+//     }
+// }
+
 #include <iostream>
 #include <vector>
+
 using namespace std;
 int n, m;
 vector<int> v;
 
-void bt(int si) {
-    v.push_back(si);
-    if(v.size() == m) {
-        for(int i = 0; i < m; i++) {
-            cout << v[i] << " ";
-        }
+void go(int cnt) {
+    if(cnt == m) {
+        for(int i : v) cout << i << ' ';
         cout << '\n';
-    }else {
-        for(int i = 1; i <= n; i++) {
-            bt(i);
-        }
+        return;
     }
-    v.pop_back();
+    for(int i = 1; i <= n; i++) {
+        v.push_back(i);
+        go(cnt+1);
+        v.pop_back();
+    }
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cout.tie(NULL); cin.tie(NULL);
     cin >> n >> m;
-    for(int i = 1; i <= n; i++) {
-        bt(i);
-    }
+    go(0);
+    return 0;
 }
