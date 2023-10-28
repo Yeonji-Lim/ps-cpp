3b8e74516a37197334e56e47e32843ab9e37fdad
Yeonji-Lim <view7186@naver.com>
2023-10-28 22:51:01 +0900

    Failed - 이전 코드는 모든 접두사를 다 넣어서 문제

--- Programmers/PGS_42577_전화번호목록.cpp
+++ Programmers/PGS_42577_전화번호목록.cpp
@@ -9,8 +9,8 @@ bool solution(vector<string> phone_book) {
     for(string phone : phone_book ) {
         for(int i = 1; i < phone.length(); i++) {
             if(s.find(phone.substr(0, i)) != s.end()) return false;
-            s.insert(phone.substr(0, i));
         }
+        s.insert(phone);
     }
     return true;
 }
