7d8f2a54789cfa2bc558f62127bef2d90c4382d6
Yeonji-Lim <view7186@naver.com>
2023-10-28 22:54:46 +0900

    Solved

--- Programmers/PGS_42577_전화번호목록.cpp
+++ Programmers/PGS_42577_전화번호목록.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 bool solution(vector<string> phone_book) {
     set<string> s;
+    for(string phone : phone_book ) s.insert(phone);
     for(string phone : phone_book ) {
         for(int i = 1; i < phone.length(); i++) {
             if(s.find(phone.substr(0, i)) != s.end()) return false;
         }
-        s.insert(phone);
     }
     return true;
 }
