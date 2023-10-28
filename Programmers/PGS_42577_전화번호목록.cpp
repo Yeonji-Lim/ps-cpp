7426fb81bf4601f610bdeb6460bdc2f93192d112
Yeonji-Lim <view7186@naver.com>
2023-10-28 22:46:00 +0900

    Failed

---
+++ Programmers/PGS_42577_전화번호목록.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    set<string> s;
+    for(string phone : phone_book ) {
+        for(int i = 1; i < phone.length(); i++) {
+            if(s.find(phone.substr(0, i)) != s.end()) return false;
+            s.insert(phone.substr(0, i));
+        }
+    }
+    return true;
+}
