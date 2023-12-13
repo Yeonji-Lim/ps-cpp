7b490091ee7b225ca40de50e3850abecafb1ab43
Yeonji-Lim <view7186@naver.com>
2023-12-13 12:45:22 +0900

    Solved - faster solution

--- LeetCode/TOP_Interview_150/LC_13.cpp
+++ LeetCode/TOP_Interview_150/LC_13.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    map<string, string> m = {
-        {"CM", "DCCCC"},
-        {"CD", "CCCC"},
-        {"XC", "LXXXX"},
-        {"XL", "XXXX"},
-        {"IX", "VIIII"},
-        {"IV", "IIII"}
+    map<string, int> m = {
+        {"CM", 900},
+        {"CD", 400},
+        {"XC", 90},
+        {"XL", 40},
+        {"IX", 9},
+        {"IV", 4}
     };
     map<char, int> cal = {
         {'I', 1},
@@ -23,18 +23,43 @@ public:
         for(i = 0; i < s.length()-1; i++) {
             auto iter = m.find(s.substr(i, 2));
             if(iter != m.end()) {
-                str += iter->second;
+                ans += iter->second;
                 i++;
             } else {
-                str += s[i];
+                ans += cal.find(s[i])->second;
                 if(i == s.length()-2) {
-                    str += s[++i];
+                    ans += cal.find(s[++i])->second;
                 }
             }
         }
-        if(i == s.length()-1) str += s[s.length()-1];
-        for(i = 0; i < str.length(); i++) {
-            ans += cal.find(str[i])->second;
+        if(i == s.length()-1) 
+            ans += cal.find(s[s.length()-1])->second;
+        return ans;
+    }
+};
+
+/*faster*/
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]) {
+                ans -= m[s[i]];
+            } else {
+                ans += m[s[i]];
+            }
         }
         return ans;
     }
